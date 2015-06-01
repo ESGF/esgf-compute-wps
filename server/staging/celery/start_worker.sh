bad2bc17cc5a8209a9e088eedd3490d0811c9c9d
Thomas Maxwell <thomas.maxwell@nasa.gov>
2015-06-01 15:59:57 -0400

    Generalize start worker sript

--- server/staging/celery/start_worker.sh
+++ server/staging/celery/start_worker.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-
-celery -A staging.celery.manager worker --loglevel=debug  --concurrency 1 -n worker.%h &
-
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <engine>"
+else
+  if [ $1 = 'spark' ]; then
+    spark-submit  --master 'local[1]'  staging/celery/worker.py
+  else
+    celery -A staging.celery.manager worker --loglevel=debug  --concurrency 1 -n worker.manager &
+  fi
