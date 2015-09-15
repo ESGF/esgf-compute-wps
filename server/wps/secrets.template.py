cd458113cee0dede40c343c854e659bef96c5c47
sashakames <sasha@llnl.gov>
2015-09-15 14:49:35 -0700

    slurm working on apache

---
+++ server/wps/secrets.template.py
@@ -0,0 +1,9 @@
+"""
+SECRET SERVER THINGS
+"""
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'YOUR SECRET KEY GOES HERE DO NOT SHARE OR PUT IN REPO'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
