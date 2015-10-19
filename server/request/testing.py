fa189a4a277d39c5ccdb6431cfc29ea119a4c9be
Thomas Maxwell <thomas.maxwell@nasa.gov>
2015-10-19 17:55:16 -0400

    Make average tests in request mgr

--- server/request/testing.py
+++ server/request/testing.py
@@ -15,19 +15,20 @@ class KernelTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def xtest01(self):
-        result_data = [   -1.405364990234375, -1.258880615234375, 0.840728759765625, 2.891510009765625,  -18.592864990234375, -11.854583740234375, -3.212005615234375, -5.311614990234375, 5.332916259765625, -1.698333740234375, 8.750885009765625 ]
+    def test01_average(self):
+        test_result = [ 78.51173231823779, 78.84015402080719, 78.29642396331322, 77.87053204967265, 77.86940510900665, 78.79289460932043, 79.00662438505067, 78.73974144579576, 78.20356569815867, 78.31290437885096 ]
         request_parms = {'version': [u'1.0.0'], 'service': [u'WPS'], 'embedded': [u'true'], 'rawDataOutput': [u'result'], 'identifier': [u'cdas'], 'request': [u'Execute'] }
-        request_parms['datainputs'] = [u'[domain=[{"id":"upper","level":{"start":0,"end":1,"system":"indices"},"longitude":-44.1,"latitude":28.0,"time":"2010-01-16T12:00:00"},{"id":"lower","level":{"start":2,"end":3,"system":"indices"},"longitude":-44.1,"latitude":28.0,"time":"2010-01-16T12:00:00"}];variable={"dset":"MERRA/mon/atmos","id":"v0:hur","domain":"upper"};operation=["CDTime.departures(v0,slice:t)","CDTime.climatology(v0,slice:t,bounds:annualcycle)"];]']
-        response_json = taskManager.processRequest( request_parms )
-        responses = json.loads(response_json)
-        for iR, result in enumerate(responses):
-            response_data = result['data']
-            self.assertEqual( result_data, response_data[0:len(result_data)] )
-
-    def xtest02(self):
-        request_parms = {'version': [u'1.0.0'], 'service': [u'WPS'], 'async': [u'true'], 'rawDataOutput': [u'result'], 'identifier': [u'cdas'], 'request': [u'Execute'] }
-        request_parms['datainputs'] = [u'[domain=[{"id":"upper","level":{"start":0,"end":1,"system":"indices"}},{"id":"lower","level":{"start":2,"end":3,"system":"indices"}}];variable={"dset":"MERRA/mon/atmos","id":"v0:hur","domain":"upper"};operation=["CDTime.departures(v0,slice:t)"];]']
+        request_parms['datainputs'] = [u'[domain=[{"id":"d0","level":{"start":0,"end":1,"system":"indices"}}];variable={"dset":"MERRA/mon/atmos","id":"v0:hur","domain":"d0"};operation=["CWT.average(v0,axis:xy)"];]']
         response_json = taskManager.processRequest( request_parms )
         response = json.loads(response_json)
-        print response
+        response_data = response[0]['data']
+        self.assertEqual( test_result, response_data[0:len(test_result)] )
+
+    def test02_ensemble_average(self):
+        test_result = [ 287.3746163535607, 287.32201028468666, 288.0899054125938, 288.8933610641959, 289.69324597969796, 290.6019424186093, 290.5856389458788, 290.3434735042316, 290.11871301777, 289.1724024490968 ]
+        request_parms = {'version': [u'1.0.0'], 'service': [u'WPS'], 'embedded': [u'true'], 'rawDataOutput': [u'result'], 'identifier': [u'cdas'], 'request': [u'Execute'] }
+        request_parms['datainputs'] = [u'[domain=[{"id":"d0","level":{"start":0,"end":1,"system":"indices"}}];variable=[{"dset":"MERRA/mon/atmos","id":"v0:ta","domain":"d0"},{"dset":"CFSR/mon/atmos","id":"v0:ta","domain":"d0"}];operation=["CWT.average(*,axis:exy)"];]']
+        response_json = taskManager.processRequest( request_parms )
+        response = json.loads(response_json)
+        response_data = response[0]['data']
+        self.assertEqual( test_result, response_data[0:len(test_result)] )
