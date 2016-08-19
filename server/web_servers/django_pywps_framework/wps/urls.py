4d844902877dd860eb3422f4e9ee1a3118eb7ca2
Jason Boutte <boutte.jason@gmail.com>
2016-08-19 16:35:53 -0700

    Adds missing fixed paths. Removes unused views/routes.

--- server/web_servers/django_pywps_framework/wps/urls.py
+++ server/web_servers/django_pywps_framework/wps/urls.py
@@ -1,19 +1,11 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-import views
-import os
-print os.path.join(os.path.dirname(__file__),"..","test_urls.html"),
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wps.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from django.conf.urls import url
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login'),
+import views
+
+urlpatterns = [
+    url(r'^$', views.view_main),
     url(r'^wps/', views.wps),
-    url(r'^postprocess/', views.postprocess),
     url(r'^status/',views.status),
     url(r'^clear/(\d+)',views.clear_process),
     url(r'^view/(\d+)',views.view_process),
-    url(r'^$', views.view_main),
-)
+]
