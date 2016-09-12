6a747cf2d65a8e77e9f0b826be2080ef09a1f419
Jason Boutte <boutte.jason@gmail.com>
2016-09-12 10:45:43 -0700

    Fixes url to support status xml and output files.

--- compute/wps/urls.py
+++ compute/wps/urls.py
@@ -5,6 +5,6 @@ import views
 urlpatterns = [
     url(r'^$', views.view_main),
     url(r'^api/processes$', views.api_processes),
-    url(r'^wps/status/(?P<file_name>.*\.xml)$', views.status),
+    url(r'^wps/status/(?P<file_name>.*(\.xml)?)$', views.status),
     url(r'^wps/', views.wps),
 ]
