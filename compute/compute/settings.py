82a932a9cd06c23bce6bdd0511cdcefc53fc5ed1
Jason Boutte <boutte.jason@gmail.com>
2016-10-07 15:50:07 -0700

    Adds logging configuration.

--- compute/compute/settings.py
+++ compute/compute/settings.py
@@ -125,3 +125,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(levelname)s][%(asctime)s][%(funcName)s (%(lineno)s) %(pathname)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'propagate': True,
+        }
+    }
+}
