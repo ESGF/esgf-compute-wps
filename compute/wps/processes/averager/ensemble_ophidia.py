5ca9824e6d7e3e3af75a925502f88d273bc07584
Jason Boutte <boutte.jason@gmail.com>
2016-10-24 07:55:02 -0700

    Removes unused file.

--- compute/wps/processes/averager/ensemble_ophidia.py
+++
@@ -1,5 +0,0 @@
-from wps.processes import esgf_operation
-
-class EnsembleOphidiaAverage(esgf_operation.ESGFOperation):
-    def __init__(self):
-        super(EnsembleOphidiaAverager, self).__init__()
