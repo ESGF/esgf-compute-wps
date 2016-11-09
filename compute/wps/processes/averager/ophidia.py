54295349818d2bf3d6e9723c9161e5bf0e0a7ac1
Jason Boutte <boutte.jason@gmail.com>
2016-11-09 15:24:45 -0800

    Updates averager/ophidia to use new OphidiaOperation class and updates settings.

--- compute/wps/processes/averager/ophidia.py
+++ compute/wps/processes/averager/ophidia.py
@@ -1,18 +1,10 @@
-import json
-import os
-import tempfile
-from uuid import uuid4 as uuid
-
-import esgf
-from PyOphidia import client
-from pywps import config
+import uuid
 
 from wps import logger
 from wps.conf import settings
-from wps.processes import data_manager
-from wps.processes import esgf_operation
+from wps.processes import ophidia_operation
 
-class OphidiaAverager(esgf_operation.ESGFOperation):
+class OphidiaAverager(ophidia_operation.OphidiaOperation):
     def __init__(self):
         super(OphidiaAverager, self).__init__()
 
@@ -21,128 +13,43 @@ class OphidiaAverager(esgf_operation.ESGFOperation):
         return 'Ophidia Average'
 
     def __call__(self, data_manager, status):
-        oph_user = settings.OPH_USER
-        oph_pass = settings.OPH_PASSWORD
-        oph_host = settings.OPH_HOST
-        oph_port = settings.OPH_PORT
+        data = self.input()[0]
 
-        cl = client.Client(oph_user, oph_pass, oph_host, oph_port)
+        metadata = data_manager.metadata(data)
 
-        if not cl.last_response:
-            raise esgf.WPSServerError('Could not connect to %s' % (oph_host,))
+        axis = [x.id for x in metadata.getAxisList()]
 
-        try:
-            container = self._create_container(cl, data_manager)
-        except esgf.WPSServerError:
-            container = 'wps'
+        container = self.createcontainer('wps', '|'.join(axis))
 
-        import_cube = self._importnc(cl, container)
+        logger.debug('Created containers "%s"', container)
 
-        status('Imported input "%s"' %
-               (self.input()[0].uri,))
+        axes = self.parameter('axes')
 
-        reduce_cube = self._reduce(cl, import_cube)
+        imp_dim = '|'.join(axes.values)
 
-        status('Reduced across "%s" dimensions' %
-               (', '.join(self.parameter('axes').values),))
+        src_cube = self.importnc(container, data.uri, data.var_name, imp_dim)
 
-        output = self._export(cl, reduce_cube, container)
+        logger.debug('Imported "%s" with implicit dimensions "%s"',
+                     data.uri, imp_dim)
 
-        status('Exported output to %s' % (output,))
+        avg_cube = self.reduce(src_cube, 'avg')
 
-        self.set_output(output, '')
+        logger.debug('Averaged over "%s" dimensions', imp_dim)
 
-    def _export(self, client, cube, container):
-        cmd = 'oph_exportnc cube=%s;output_path=%s;output_name=%s;'
+        filename = str(uuid.uuid4())
 
-        output_path = config.getConfigValue('server', 'outputPath', '/var/wps')
+        self.exportnc2(avg_cube, settings.OPH_DAP_PATH, filename)
 
-        output_name = '%s.nc' % (str(uuid()),)
+        logger.debug('Exported "%s" to "%s.nc"',
+                     settings.OPH_DAP_PATH,
+                     filename)
 
-        client.submit(cmd % (cube,
-                             output_path,
-                             output_name))
+        uri_args = {
+            'hostname': settings.OPH_HOST,
+            'port': settings.OPH_DAP_PORT,
+            'filename': '%s.nc' % (filename,),
+        }
 
-        self._check_status(client)
+        output_uri = settings.OPH_DAP_PATH_FORMAT.format(**uri_args)
 
-        output = os.path.join(output_path, output_name)
-
-        logger.debug('Done exporting to "%s"', output)
-
-        return output
-
-    def _reduce(self, client, cube):
-        cmd = 'oph_reduce cube=%s;operation=avg;'
-
-        client.submit(cmd % (cube, ))
-
-        self._check_status(client)
-
-        cube = self._parse_cube(client, 'reduce')
-
-        logger.debug('Reduce "avg" finished.')
-
-        return cube
-
-    def _importnc(self, client, container):
-        variable = self.input()[0]
-        
-        cmd = 'oph_importnc container=%s;measure=%s;src_path=%s;'
-
-        client.submit(cmd % (container,
-                             variable.var_name,
-                             variable.uri))
-
-        self._check_status(client)
-    
-        cube = self._parse_cube(client, 'importnc')
-
-        logger.debug('Imported "%s" to %s', variable.uri, cube)
-
-        return cube
-
-    def _create_container(self, client, data_manager):
-        var = data_manager.metadata(self.input()[0])
-
-        dim = '|'.join([x.id for x in var.getAxisList()])
-
-        imp_dim = '|'.join(self.parameter('axes').values)
-
-        cmd = 'oph_createcontainer container=%s;dim=%s;imp_dim=%s;'
-
-        container = 'wps'
-
-        client.submit(cmd % (container,
-                             dim,
-                             imp_dim))
-
-        self._check_status(client)
-
-        logger.debug('Created container "%s"', container)
-
-        return container
-
-    def _parse_cube(self, client, key):
-        json_doc = self._parse_response(client)
-
-        task = [x for x in json_doc['response'] if x['objkey'] == key]
-
-        cube = [x['message'] for x in task[0]['objcontent'] if 'message' in x]
-
-        return cube[0]
-
-    def _check_status(self, client):
-        json_doc = self._parse_response(client)
-
-        status = [x['objcontent'][0]['title']
-                  for x in json_doc['response']
-                  if x['objkey'] == 'status']
-
-        if not status or status[0].lower() != 'success':
-            raise esgf.WPSServerError('Ophidia command "%s" failed.' % (client.last_request,))
-
-    def _parse_response(self, client):
-        if not client.last_response:
-            raise esgf.WPSServerError('Ophidia command "%s" returned no response.' % (client.last_request,))
-
-        return json.loads(client.last_response)
+        self.set_output(output_uri, data.var_name)
