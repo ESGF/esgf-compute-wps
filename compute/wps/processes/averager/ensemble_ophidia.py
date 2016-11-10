33af3d250f3910345ab19f737ba2438fcb87bf01
Jason Boutte <boutte.jason@gmail.com>
2016-11-10 12:51:35 -0800

    Updates ophidia ensemble averager to use new OphidiaOperation

--- compute/wps/processes/averager/ensemble_ophidia.py
+++ compute/wps/processes/averager/ensemble_ophidia.py
@@ -1,17 +1,10 @@
-import json
-import os
 import uuid
 
-from PyOphidia import client
-from pywps import config
-import esgf
-
-from wps.processes import data_manager
-from wps.processes import esgf_operation
 from wps import logger
 from wps.conf import settings
+from wps.processes import ophidia_operation
 
-class EnsembleAveragerOphidia(esgf_operation.ESGFOperation):
+class EnsembleAveragerOphidia(ophidia_operation.OphidiaOperation):
     def __init__(self):
         super(EnsembleAveragerOphidia, self).__init__()
 
@@ -19,141 +12,67 @@ class EnsembleAveragerOphidia(esgf_operation.ESGFOperation):
     def title(self):
         return 'Ophidia Ensemble Averager'
 
-    def oph_submit(self, client, cmd, ignore_resp=False):
-        class OphResponseWrapper(object):
-            def __init__(self, response):
-                self._data = json.loads(response)
-
-            @property
-            def success(self):
-                status = self._find_by_objkey('status')
-
-                return status[0]['title'].lower() == 'success'
-
-            def datacube(self, key):
-                data = self._find_by_objkey(key) 
-
-                return data[0]['message']
-
-            def _find_by_objkey(self, key):
-                try:
-                    return [x['objcontent'] for x in self._data['response']
-                            if x['objkey'] == key][0]
-                except IndexError:
-                    return None
-
-        client.submit(cmd)
-
-        if ignore_resp:
-            return
-
-        if not client.last_response:
-            raise esgf.WPSServerError('Ophidia return no response from "%s"' %
-                                      (client.last_request,))
-
-        return OphResponseWrapper(client.last_response)
-
     def __call__(self, data_manager, status):
         if len(self.input()) < 2:
             raise esgf.WPSServerError('Must supply two input variables.')
 
-        oph_host = settings.OPH_HOST
-        oph_port = settings.OPH_PORT
-        oph_user = settings.OPH_USER
-        oph_pass = settings.OPH_PASSWORD
+        data_list = self.input()
 
-        cl = client.Client(oph_user, oph_pass, oph_host, oph_port)
+        metadata = data_manager.metadata(data_list[0])
 
-        input0 = self.input()[0]
+        axis = [x.id for x in metadata.getAxisList()]
 
-        var0 = data_manager.metadata(input0)
+        container = self.createcontainer('wps', '|'.join(axis))
 
-        axis_ids = [x.id for x in var0.getAxisList()]
+        entries = self.list(level=3, container=container)
 
-        dim = '|'.join(axis_ids)
+        inp_cubes = []
 
-        container = 'wps_%s' % ('_'.join(axis_ids))
+        for data in data_list:
+            cubes = entries.filter_by_src(data.uri)
 
-        self.oph_submit(cl, 'oph_createcontainer container=%s;dim=%s;' %
-                        (container, dim), True)
+            if not len(cubes):
+                cube = self.importnc(container, data.uri, data.var_name)
+                
+                inp_cubes.append(cube)
 
-        status('Created container "%s" with dimensions "%s"' %
-               (container, dim))
+                logger.debug('Imported "%s" into cube "%s"', data.uri, cube)
+            else:
+                # Assume ordered chronologically
+                inp_cubes.append(cubes[-1][2])
 
-        cubes = []
-
-        for inp in self.input():
-            resp = self.oph_submit(cl, 'oph_importnc container=%s;measure=%s;'
-                                   'src_path=%s;' %
-                                   (container, inp.var_name, inp.uri))
-
-            if not resp.success:
-                raise esgf.WPSServerError('Ophidia failed to import "%s"' %
-                                          (inp.uri,))
-
-            cubes.append(resp.datacube('importnc'))
-
-            status('Done importing "%s" to "%s"' %
-                   (inp.uri, resp.datacube('importnc')))
+                logger.debug('"%s" already imported into cube "%s"',
+                             data.uri,
+                             cubes[-1][2])
 
 
-        resp = self.oph_submit(cl, 'oph_intercube cube=%s;cube2=%s;'
-                               'operation=sum;output_measure=%s;' %
-                               (cubes[0], cubes[1], 'tas'))
+        sum_cube = self.intercube(inp_cubes[0],
+                                  inp_cubes[1],
+                                  'sum',
+                                  'summation')
 
-        if not resp.success:
-            raise esgf.WPSServerError('Ophidia failed initial intercube '
-                                      'operation')
+        for next_cube in inp_cubes[2:]:
+            sum_cube = self.intercube(sum_cube, next_cube, 'sum', 'summation')
 
-        curr_cube = resp.datacube('intercube')
+        factor = 1.0/len(inp_cubes)
 
-        for next_cube in cubes[2:]:
-            resp = self.oph_submit(cl, 'oph_intercube cube=%s;cube2=%s;'
-                                   'operation=sum;output_measure=%s;' %
-                                   (curr_cube, next_cube, 'tas'))
+        new_measure = 'ensemble_avg'
 
-            if not resp.success:
-                raise esgf.WPSServerError('Ophidia failed initial intercube '
-                                          'operation')
+        avg_cube = self.apply(sum_cube,
+                              query='oph_mul_scalar(\'OPH_FLOAT\', '
+                              '\'OPH_FLOAT\', measure, %f)' % (factor,),
+                              measure=new_measure)
 
-            curr_cube = resp.datacube('intercube')
+        filename = str(uuid.uuid4())
 
-            status('Done intercube sum operation')
+        self.exportnc2(avg_cube, settings.OPH_DAP_PATH, filename)
 
-        divisor = 1.0/len(cubes)
+        uri_args = {
+            'hostname': settings.OPH_HOST,
+            'port': settings.OPH_DAP_PORT,
+            'filename': '%s.nc' % (filename, ),
+        }
 
-        resp = self.oph_submit(cl, 'oph_apply cube=%s;query=oph_mul_scalar'
-                               '(\'oph_float\', \'oph_float\', measure, %f);'
-                               % (curr_cube, divisor))
+        output_uri = settings.OPH_DAP_PATH_FORMAT.format(**uri_args)
 
-        if not resp.success:
-            raise esgf.WPSServerError('Ophidia apply operation failed.')
-
-        avg_cube = resp.datacube('apply')
-
-        status('Finished element wise multiplication by %s' % (divisor,))
-
-        resp = self.oph_submit(cl, 'oph_merge cube=%s;' % (avg_cube,))
-
-        if not resp.success:
-            raise esgf.WPSServerError('Ophidia merge operation failed.')
-
-        merged_cube = resp.datacube('merge')
-
-        status('Finished merging fragments')
-
-        output_path = config.getConfigValue('server', 'outputPath', '/var/wps')
-        output_name = str(uuid.uuid4())
-
-        resp = self.oph_submit(cl, 'oph_exportnc cube=%s;output_path=%s;'
-                               'output_name=%s;' %
-                               (merged_cube, output_path, output_name))
-
-        if not resp.success:
-            raise esgf.WPSServerError('Ophidia export operation failed.')
-
-        status('Finished exporting file')
-
-        output_file = os.path.join(output_path, '%s.nc' % (output_name,)) 
-
-        self.set_output(output_file, 'tas')
+        self.set_output(output_uri, new_measure)
