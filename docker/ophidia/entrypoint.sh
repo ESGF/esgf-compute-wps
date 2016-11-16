e08c341a8ec96b0165ca308aeed78392f11d41a7
Jason Boutte <boutte.jason@gmail.com>
2016-11-16 08:47:01 -0800

    Updates ophidia dockerfile

--- docker/ophidia/entrypoint.sh
+++ docker/ophidia/entrypoint.sh
@@ -1,98 +1,140 @@
 #!/bin/bash
 
-pass="abcd"
-
 base="/root/miniconda2"
-cert="."
 
-root_key="$cert/rootkey.pem"
-root_req="$cert/rootreq.pem"
-root_cert="$cert/rootcert.pem"
-ca_cert="$cert/cacert.pem"
+passwd="abcd"
 
-srv_key="$cert/serverkey.pem"
-srv_req="$cert/serverreq.pem"
-srv_cert="$cert/servercert.pem"
-my_srv="$cert/myserver.pem"
+hostname=$(hostname)
 
-ssh-keygen -t dsa -f ~/.ssh/id_dsa -N ""
+function mysql_setup {
+  service mysqld start
 
-cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
+  /usr/bin/mysqladmin -u root password "$passwd"
 
-chmod 0600 ~/.ssh/authorized_keys
+  mysql -u root --password=abcd mysql < "$base/etc/create_func.sql"
+  mysql -u root --password=abcd mysql -e "create database ophidiadb;"
+  mysql -u root --password=abcd mysql -e "create database oph_dimensions;"
+  mysql -u root --password=abcd ophidiadb < "$base/etc/ophidiadb.sql"
+  mysql -u root --password=abcd ophidiadb -e "INSERT INTO host (hostname, cores, memory) VALUES ('127.0.0.1', 4, 1);"
+  mysql -u root --password=abcd ophidiadb -e "INSERT INTO dbmsinstance (idhost, login, password, port) VALUES (1, 'root', '$passwd', 3306);"
+  mysql -u root --password=abcd ophidiadb -e "INSERT INTO hostpartition (partitionname) VALUES ('test');"
+  mysql -u root --password=abcd ophidiadb -e "INSERT INTO hashost (idhostpartition, idhost) VALUES (1, 1);"
 
-openssl req -newkey rsa:1024 -passout pass:$pass -subj "/" -sha1 -keyout \
-  $root_key -out $root_req
+cat << EOF > "$base/etc/ophidiadb.conf"
+MAPPER_DB_NAME=ophidiadb
+MAPPERDB_HOST=$hostname
+MAPPERDB_PORT=3306
+MAPPERDB_LOGIN=root
+MAPPERDB_PWD=$passwd
+EOF
+}
 
-openssl x509 -req -in $root_req -passin pass:$pass -sha1 -extensions v3_ca \
-  -signkey $root_key -out $root_cert
+function create_dir {
+  if [[ ! -e "$1" ]]
+  then
+    mkdir -p "$1"
+  fi
+}
 
-cat $root_cert $root_key > $ca_cert
-
-openssl req -newkey rsa:1024 -passout pass:$pass -subj "/" -sha1 -keyout \
-  $srv_key -out $srv_req
-
-openssl x509 -req -in $srv_req -passin pass:$pass -sha1 -extensions usr_cert \
-  -CA $ca_cert -CAkey $ca_cert -CAcreateserial -out $srv_cert
-
-cat $srv_cert $srv_key $root_cert > $my_srv
-
-cert_base="$base/etc/cert"
-
-mkdir -p $cert_base
-
-cp $ca_cert $my_srv $cert_base
-
-cat << EOF > /root/miniconda2/etc/slurm.conf
-ControlMachine=$(hostname)
-MpiDefault=none
-ProctrackType=proctrack/pgid
-ReturnToService=1
-SlurmctldPidFile=/var/run/slurmctld.pid
-SlurmdPidFile=/var/run/slurmd.pid
-SlurmdSpoolDir=/var/spool/slurmd
-SlurmUser=root
-StateSaveLocation=/var/spool
-SwitchType=switch/none
-TaskPlugin=task/none
-FastSchedule=1
-SchedulerType=sched/backfill
-SelectType=select/cons_res
-SelectTypeParameters=CR_CPU
-AccountingStorageType=accounting_storage/none
-ClusterName=cluster
-JobAcctGatherType=jobacct_gather/none
-NodeName=$(hostname) CPUs=4 State=UNKNOWN
-PartitionName=debug Nodes=$(hostname) Default=YES MaxTime=INFINITE State=UP
+function framework_configs {
+cat << EOF > "$base/etc/oph_configuration"
+MAPPER_DB_NAME=ophidiadb
+MAPPERDB_HOST=$hostname
+MAPPERDB_PORT=3306
+MAPPERDB_LOGIN=root
+MAPPERDB_PWD=$passwd
+WEB_SERVER=http://$hostname/site    ← Use *protocol://hostname/subfolders*
+WEB_SERVER_LOCATION=/var/www/html/site
+MEMORY=2048
+BASE_SRC_PATH=/data/repository
 EOF
 
-echo ${CPU_COUNT}
+cat << EOF > "$base/etc/oph_dim_configuration"
+MAPPER_DB_NAME=oph_dimensions
+MAPPERDB_HOST=$hostname
+MAPPERDB_PORT=3306
+MAPPERDB_LOGIN=root
+MAPPERDB_PWD=$passwd
+EOF
 
-sed -ibak "s/\(MAPPERDB_PWD\)=.*/\1=abcd/g" /root/miniconda2/etc/oph_configuration
-sed -ibak "s/\(MAPPERDB_PWD\)=.*/\1=abcd/g" /root/miniconda2/etc/oph_dim_configuration
-sed -ibak "s/\(SUBM_USER_PUBLK\)=.*/\1=\/root\/\.ssh\/id_dsa\.pub/g" /root/miniconda2/etc/server.conf
-sed -ibak "s/\(SUBM_USER_PRIVK\)=.*/\1=\/root\/\.ssh\/id_dsa/g" /root/miniconda2/etc/server.conf
+cat << EOF > "$base/etc/oph_soap_configuration"
+MAPPER_DB_NAME=oph_dimensions
+MAPPERDB_HOST=$hostname
+MAPPERDB_PORT=3306
+MAPPERDB_LOGIN=root
+MAPPERDB_PWD=$passwd
+EOF
+}
 
-service mysqld start
+function server_configs {
+  openssl req -newkey rsa:1024 -passout pass:$passwd -subj "/" -sha1 \
+    -keyout rootkey.pem -out rootreq.pem
 
-mysqladmin -h 127.0.0.1 -u root password $pass
+  openssl x509 -req -in rootreq.pem -passin pass:$passwd -sha1 \
+    -extensions v3_ca -signkey rootkey.pem -out rootcert.pem
 
-mysql -h 127.0.0.1 -u root --password=abcd mysql < "$base/etc/create_func.nomatheval.sql"
-#mysql -h 127.0.0.1 -u root --password=abcd
-mysql -h 127.0.0.1 -u root --password=abcd mysql -e "create database ophidiadb;"
-mysql -h 127.0.0.1 -u root --password=abcd mysql -e "create database oph_dimensions;"
-mysql -h 127.0.0.1 -u root --password=abcd ophidiadb < "$base/etc/ophidiadb.sql"
-mysql -h 127.0.0.1 -u root --password=abcd ophidiadb -e "INSERT INTO host (hostname, cores, memory) VALUES ('127.0.0.1', 4, 1);"
-mysql -h 127.0.0.1 -u root --password=abcd ophidiadb -e "INSERT INTO dbmsinstance (idhost, login, password, port) VALUES (1, 'root', 'abcd', 3306);"
-mysql -h 127.0.0.1 -u root --password=abcd ophidiadb -e "INSERT INTO hostpartition (partitionname) VALUES ('test');"
-mysql -h 127.0.0.1 -u root --password=abcd ophidiadb -e "INSERT INTO hashost VALUES (1, 1);"
+  cat rootcert.pem rootkey.pem > cacert.pem
 
-/root/miniconda2/sbin/slurmctld
-/root/miniconda2/sbin/slurmd
+  openssl req -newkey rsa:1024 -passout pass:$passwd -subj "/" -sha1 \
+    -keyout serverkey.pem -out serverreq.pem
 
-service munge start
-service sshd start
+  openssl x509 -req -in serverreq.pem -passin pass:$passwd -sha1 \
+    -extensions usr_cert -CA cacert.pem -CAkey cacert.pem -CAcreateserial \
+    -out servercert.pem
 
-oph_server &>/dev/null &
+  cat servercert.pem serverkey.pem rootcert.pem > myserver.pem
+
+  create_dir "$base/etc/cert"
+
+  cp myserver.pem cacert.pem "$base/etc/cert"
+
+cat << EOF > "$base/etc/server.conf"
+TIMEOUT=3600
+INACTIVITY_TIMEOUT=31536000
+WORKFLOW_TIMEOUT=86400
+LOGFILE=$base/log/server.log
+CERT=$base/etc/cert/myserver.pem
+CA=$base/etc/cert/cacert.pem
+CERT_PASSWORD=abcd
+RMANAGER_CONF_FILE=$base/etc/rmanager.conf
+AUTHZ_DIR=$base/authz
+TXT_DIR=$base/txt
+WEB_SERVER=http://$hostname/ophidia
+WEB_SERVER_LOCATION=/var/www/html/ophidia
+OPERATOR_CLIENT=$base/bin/oph_analytics_framework
+IP_TARGET_HOST=$hostname
+SUBM_USER=ophidia
+SUBM_USER_PUBLK=/root/.ssh/id_dsa.pub
+SUBM_USER_PRIVK=/root/.ssh/id_dsa
+OPH_XML_URL=http://$hostname/ophidia/operators_xml
+OPH_XML_DIR=$base/etc/operators_xml
+NOTIFIER=framework
+SERVER_FARM_SIZE=16
+QUEUE_SIZE=0
+HOST=$hostname
+PORT=11732
+PROTOCOL=https
+AUTO_RETRY=3
+POLL_TIME=60
+BASE_SRC_PATH=/data/repository
+BASE_BACKOFF=1
+EOF
+
+  srv_conf=$(oph_server 2>&1 | grep -e "configuration" | cut -d' ' -f8 | tr -d "'")
+
+  mkdir -p $(dirname $srv_conf)
+
+  ln -sf "$base/etc/server.conf" "$srv_conf"
+}
+
+create_dir "$base/log"
+create_dir "$base/txt"
+create_dir "$base/authz/sessions"
+
+framework_configs
+
+server_configs
+
+mysql_setup
 
 exec "$@"
--- docker/ophidia/oph_term.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source activate oph-term
-
-oph_term -H 127.0.0.1 -P 11732 -u oph-test -p abcd
