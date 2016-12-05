60bad853d3337f8edf5893a789e3d4d3d2c267c7
Jason Boutte <boutte.jason@gmail.com>
2016-12-05 07:25:41 +0000

    Adds regridding to cdat ensemble averager

--- compute/wps/processes/cdat/ensemble.py
+++ compute/wps/processes/cdat/ensemble.py
@@ -1,4 +1,5 @@
 import cdms2
+from cdms2.selectors import timeslice
 import esgf
 import os
 from pywps import config
@@ -8,9 +9,14 @@ from wps import logger
 from wps.processes import esgf_operation
 from wps.processes.esgf_operation import create_from_def
 
-from memory_profiler import profile
-
 class CDATEnsemble(esgf_operation.ESGFOperation):
+    KNOWN_GRIDS = {
+        't42': {
+            'func': cdms2.createGaussianGrid,
+            'args': [128]
+        }
+    }
+
     def __init__(self):
         super(CDATEnsemble, self).__init__()
 
@@ -19,7 +25,14 @@ class CDATEnsemble(esgf_operation.ESGFOperation):
         return 'CDAT Ensemble'
 
     def __call__(self, data_manager, status):
-        src = [data_manager.metadata(x) for x in self.input()
+        var = [x for x in self.input() if isinstance(x, esgf.Variable)]
+
+        var_name = None
+
+        if len(var):
+            var_name = var[0].var_name
+
+        src = [data_manager.metadata(x) for x in var
                if isinstance(x, esgf.Variable)]
 
         # Find inputs that are operations and execute them appending the
@@ -32,21 +45,29 @@ class CDATEnsemble(esgf_operation.ESGFOperation):
 
             op = create_from_def(inp)
 
+            if self.domain:
+                op.data.domain = self.domain
+
             op.__call__(data_manager, status)
 
             src.append(data_manager.metadata(op.output))
 
+            # If we couldn't get the variable name from source files,
+            # take the first name from input operation outputs
+            if not var_name:
+                var_name = op.output.var_name
+
         # assumed all files are from the same model
         # TODO add some validation
-        time = src[0].getTime()
+        time = [x.getTime() for x in src]
 
         temporal, spatial = self._gen_domains(src[0].getAxisList(),
-                                              time,
+                                              time[0],
                                               self.domain)
         
         logger.debug('Spatial domain %r', spatial)
 
-        start, end, step = self._convert_temporal_domain(time, temporal)
+        start, end, step = self._convert_temporal_domain(time[0], temporal)
 
         logger.debug('Temporal domain: start=%s stop=%s step=%s', start, end, step)
 
@@ -58,20 +79,56 @@ class CDATEnsemble(esgf_operation.ESGFOperation):
 
         fout = cdms2.open(new_file, 'w')
 
-        var_name = self.input()[0].var_name
-
         denom = len(src)
+
+        target_grid = None
+
+        gridder = self.parameter('gridder', required=False)
+
+        # Build the target grid if gridder is passed to the operation
+        if gridder:
+            if isinstance(gridder.grid, esgf.Domain):
+                raise esgf.WPSServerError(
+                    'Target grid from Domain NotImplemented yet.')
+            elif isinstance(gridder.grid, esgf.Variable):
+                raise esgf.WPSServerError(
+                    'Target grid from Variable NotImplemented yet.')
+            elif isinstance(gridder.grid, (str, unicode)):
+                try:
+                    grid = self.KNOWN_GRIDS[gridder.grid.lower()]
+                except KeyError:
+                    raise esgf.WPSServerError('Unknown target grid %s' %
+                                              (gridder.grid,))
+
+                target_grid = grid['func'](*grid['args'])
+            else:
+                raise esgf.WPSServerError(
+                    'Unknown value passed as target grid %r' % (gridder.grid,))
+
+            logger.debug('Target grid %r', target_grid)
 
         for i in xrange(start, end, step):
             logger.debug('Averaging time slice %s', i)
 
-            data = [x(time=time[i], **spatial) for x in src]
+            # Grab indexed time slice relative to it's own timeline
+            # Apply spatial subset if present
+            data = [x(time=time[j][i], **spatial) for j, x in enumerate(src)]
+
+            # Regrid slice
+            if target_grid:
+                data = [x.regrid(target_grid,
+                                 regridTool=gridder.tool,
+                                 regridMethod=gridder.method) for x in data]
 
             data_sum = reduce(lambda x, y: x + y, data)
 
             data_avg = data_sum / denom
 
             fout.write(data_avg, id=var_name) 
+
+            del data[:]
+            del data_sum
+            del data_avg
 
         fout.close()
 
