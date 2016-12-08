5296b9ccccf0c9e9fe2bd5bfba73e6087be6883a
Jason Boutte <boutte.jason@gmail.com>
2016-12-08 16:58:57 +0000

    Updates default settings for ophidia dap.

--- compute/wps/conf/settings.py
+++ compute/wps/conf/settings.py
@@ -7,7 +7,7 @@ WPS_CONFIG = getattr(settings, 'WPS_CONFIG', os.path.join(BASE_DIR, 'wps.cfg'))
 PROCESS_DIR = getattr(settings, 'PROCESS_DIR', os.path.join(BASE_DIR, 'processes'))
 
 DAP_PATH_FORMAT = getattr(settings, 'DAP_PATH_FORMAT', 'http://{hostname}:{port}/thredds/dodsC/test/{filename}')
-DAP_HOSTNAME = getattr(settings, 'DAP_HOSTNAME', '0.0.0.0')
+DAP_HOSTNAME = getattr(settings, 'DAP_HOSTNAME', 'thredds')
 DAP_PORT = getattr(settings, 'DAP_PORT', 8080)
 
 OPH_USER = getattr(settings, 'OPH_USER', 'oph-test')
@@ -16,6 +16,6 @@ OPH_HOST = getattr(settings, 'OPH_HOST', 'aims2.llnl.gov')
 OPH_PORT = getattr(settings, 'OPH_PORT', 11732)
 OPH_CORES = getattr(settings, 'OPH_CORES', 8)
 OPH_DAP_PATH_FORMAT = getattr(settings, 'OPH_DAP_PATH_FORMAT',
-                              'http://{hostname}:{port}/thredds/dodsC/test/{filename}.html')
+                              'http://{hostname}:{port}/thredds/dodsC/test/wps/{filename}')
 OPH_DAP_PATH = getattr(settings, 'OPH_DAP_PATH', '/export/boutte3/thredds/data/wps')
 OPH_DAP_PORT = getattr(settings, 'OPH_DAP_PORT', 9001)
