34c1663cfdcb1dfb6fd0c5205643cef79c3b3042
Jason Boutte <boutte.jason@gmail.com>
2017-01-05 15:34:52 -0800

    Removes pywps as will be replaced by https://github.com/nasa-nccs-cds/CDAS2

--- compute/wps/urls.py
+++ compute/wps/urls.py
@@ -3,8 +3,5 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^$', views.index), 
     url(r'^wps', views.wps),
-    url(r'^status/(?P<file_name>.*(\.xml)?)$', views.status),
-    url(r'^api/processes$', views.api_processes),
 ]
