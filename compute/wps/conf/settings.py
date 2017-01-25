bade4f5bb797e4a15523556ecf3e3ce01d266f39
Jason Boutte <boutte.jason@gmail.com>
2017-01-25 09:45:44 -0800

    Updates default settings for docker image.

--- compute/wps/conf/settings.py
+++ compute/wps/conf/settings.py
@@ -4,6 +4,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-CDAS_HOST = getattr(settings, 'CDAS_HOST', 'localhost')
-CDAS_REQUEST_PORT = getattr(settings, 'CDAS_REQUEST_PORT', 4360)
-CDAS_RESPONSE_PORT = getattr(settings, 'CDAS_RESPONSE_PORT', 4361)
+CDAS_HOST = getattr(settings, 'CDAS_HOST', 'cdas')
+CDAS_REQUEST_PORT = getattr(settings, 'CDAS_REQUEST_PORT', 4356)
+CDAS_RESPONSE_PORT = getattr(settings, 'CDAS_RESPONSE_PORT', 4357)
