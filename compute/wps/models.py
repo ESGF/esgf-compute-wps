7d89dc4d99e705f2e5f62b822535d2f99439acf6
Jason Boutte <boutte.jason@gmail.com>
2017-02-23 19:20:42 +0000

    Adds server model #18
    Forgot to add migrations.

--- compute/wps/models.py
+++ compute/wps/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Server(models.Model):
+    name = models.CharField(max_length=256, blank=True, null=True)
+    address = models.CharField(max_length=64, unique=True)
+    is_wps = models.BooleanField(default=False)
+    queue = models.PositiveIntegerField(default=0)
+    queue_size = models.PositiveIntegerField(default=0)
