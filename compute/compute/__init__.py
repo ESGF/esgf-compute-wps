a4d02eed58396c688e8f03d3c4d587542cd37d4e
Jason Boutte <boutte.jason@gmail.com>
2017-02-24 00:53:06 +0000

    Adds Django global celery app.

---
+++ compute/compute/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from compute.celery import app as celery_app
+
+__all__ = ['celery_app']
---
+++ compute/compute/celery.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compute.settings')
+
+app = Celery('compute',
+        backend = os.getenv('CELERY_BACKEND', 'redis://0.0.0.0'),
+        broker = os.getenv('CELERY_BROKER', 'redis://0.0.0.0'),
+        )
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
