8cfec8582d30f4b45916f8ee91100be62f19bd87
Jason Boutte <boutte.jason@gmail.com>
2017-02-24 00:53:42 +0000

    Reworked the views and tasks, still a work in progress.

--- compute/wps/tasks.py
+++ compute/wps/tasks.py
@@ -1,13 +1,18 @@
 #! /usr/bin/env python
 
 from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import threading
 
 import zmq
-import threading
-from pycdas.portal import cdas
 
-from wps import logger
-from wps.celery import app
+from esgf.wps_lib import metadata as md
+
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 class ResponseThread(threading.Thread):
     def __init__(self, context, host, response_port):
@@ -24,8 +29,24 @@ class ResponseThread(threading.Thread):
 
         self.response_socket.close()
 
-@app.task
-def get_capabilities(host, request_port, response_port):
+@shared_task
+def handle_get(request, host, request_port, response_port):
+    logger.info(request.GET)
+
+    required = ('service', 'request')
+
+    for para in required:
+        if para not in request.GET:
+            er = md.ExceptionReport('1.0.0')
+
+            er.add_exception(md.Exception.MissingParameterValue,
+                    'Missing required parameter',
+                    para)
+
+            return er.xml()
+
+    logger.info('CDAS2 host: %s request: %s response: %s', host, request_port, response_port)
+
     context = zmq.Context.instance()
 
     request_socket = context.socket(zmq.PUSH)
@@ -34,76 +55,91 @@ def get_capabilities(host, request_port, response_port):
     response_thread = ResponseThread(context, host, response_port)
     response_thread.start()
 
-    request_socket.send('12345678!getCapabilities!WPS')
+    request = request.GET['request'].lower()
+
+    if request == 'getcapabilities':
+        request_socket.send(str('12345678!getCapabilities!WPS'))
+    elif request == 'describeprocess':
+        pass
+    elif request == 'execute':
+        pass
 
     response_thread.join()
 
     request_socket.close()
 
     result = response_thread.response
+
+    logger.info(result)
 
     return result.split('!')[-1]
 
-@app.task
-def describe_process(host, request_port, response_port, identifier):
-    context = zmq.Context.instance()
+@shared_task
+def handle_post(request):
+    logger.info(request.POST)
 
-    request_socket = context.socket(zmq.PUSH)
-    request_socket.connect('tcp://%s:%s' % (host, request_port))
+    raise NotImplementedError()
 
-    response_thread = ResponseThread(context, host, response_port)
-    response_thread.start()
-
-    request_socket.send('12345678!describeProcess!%s' % (str(identifier),))
-
-    response_thread.join()
-
-    request_socket.close()
-
-    result = response_thread.response
-
-    return result
-
-@app.task
-def execute(host, request_port, response_port, identifier, data_inputs):
-    context = zmq.Context.instance()
-
-    request_socket = context.socket(zmq.PUSH)
-    request_socket.connect('tcp://%s:%s' % (host, request_port))
-
-    response_thread = ResponseThread(context, host, response_port)
-    response_thread.start()
-
-    request_socket.send('12345678!execute!%s!%s' % (str(identifier), str(data_inputs)))
-
-    response_thread.join()
-
-    request_socket.close()
-
-    result = response_thread.response
-
-    # Temporary fix, eventuall we'll completely rewrap the CDAS response
-    result_doc = result.split('!')[-1]
-
-    from lxml import etree
-
-    tree = etree.fromstring(result_doc)
-
-    # Update status to reflect ProcessSucceeded
-    old_tag = tree.xpath('/wps:ExecuteResponse/wps:Status', namespaces=tree.nsmap)[0].getchildren()[0].tag
-        
-    import re
-
-    new_tag = re.sub('Process.*', 'ProcessSucceeded', old_tag)
-
-    tree.xpath('/wps:ExecuteResponse/wps:Status', namespaces=tree.nsmap)[0].getchildren()[0].tag = new_tag
-
-    # Remove bogus output references that confuse the end-user API
-    matches = tree.xpath('/wps:ExecuteResponse/wps:ProcessOutputs/wps:Output/wps:Reference', namespaces=tree.nsmap)
-
-    for x in matches:
-        x.getparent().remove(x)
-        
-    result = etree.tostring(tree)
-
-    return result
+#@app.task
+#def describe_process(host, request_port, response_port, identifier):
+#    context = zmq.Context.instance()
+#
+#    request_socket = context.socket(zmq.PUSH)
+#    request_socket.connect('tcp://%s:%s' % (host, request_port))
+#
+#    response_thread = ResponseThread(context, host, response_port)
+#    response_thread.start()
+#
+#    request_socket.send('12345678!describeProcess!%s' % (str(identifier),))
+#
+#    response_thread.join()
+#
+#    request_socket.close()
+#
+#    result = response_thread.response
+#
+#    return result
+#
+#@app.task
+#def execute(host, request_port, response_port, identifier, data_inputs):
+#    context = zmq.Context.instance()
+#
+#    request_socket = context.socket(zmq.PUSH)
+#    request_socket.connect('tcp://%s:%s' % (host, request_port))
+#
+#    response_thread = ResponseThread(context, host, response_port)
+#    response_thread.start()
+#
+#    request_socket.send('12345678!execute!%s!%s' % (str(identifier), str(data_inputs)))
+#
+#    response_thread.join()
+#
+#    request_socket.close()
+#
+#    result = response_thread.response
+#
+#    # Temporary fix, eventuall we'll completely rewrap the CDAS response
+#    result_doc = result.split('!')[-1]
+#
+#    from lxml import etree
+#
+#    tree = etree.fromstring(result_doc)
+#
+#    # Update status to reflect ProcessSucceeded
+#    old_tag = tree.xpath('/wps:ExecuteResponse/wps:Status', namespaces=tree.nsmap)[0].getchildren()[0].tag
+#        
+#    import re
+#
+#    new_tag = re.sub('Process.*', 'ProcessSucceeded', old_tag)
+#
+#    tree.xpath('/wps:ExecuteResponse/wps:Status', namespaces=tree.nsmap)[0].getchildren()[0].tag = new_tag
+#
+#    # Remove bogus output references that confuse the end-user API
+#    matches = tree.xpath('/wps:ExecuteResponse/wps:ProcessOutputs/wps:Output/wps:Reference', namespaces=tree.nsmap)
+#
+#    for x in matches:
+#        x.getparent().remove(x)
+#        
+#    result = etree.tostring(tree)
+#
+#    return result
--- compute/wps/views.py
+++ compute/wps/views.py
@@ -3,52 +3,17 @@ from django import http
 from wps import tasks
 from wps.conf import settings
 
-class MalformedWPSRequestError(Exception):
-    pass
-
 def wps(request):
+    # Need to change to async, we're running locally right now.
     if request.method == 'GET':
-        response = handle_get(request)
-    else:
-        response = 'Not Implemented' 
+        response = tasks.handle_get(request,
+                settings.CDAS_HOST,
+                settings.CDAS_REQUEST_PORT,
+                settings.CDAS_RESPONSE_PORT)
+    elif request.method == 'POST':
+        response = task.handle_post(request,
+                settings.CDAS_HOST,
+                settings.CDAS_REQUEST_PORT,
+                settings.CDAS_RESPONSE_PORT)
 
     return http.HttpResponse(response)
-
-def handle_get(request):
-    try:
-        wps_request = request.GET['request']
-    except KeyError:
-        raise MalformedWPSRequestError()
-    else:
-        wps_request = wps_request.lower()
-
-    if wps_request == 'getcapabilities':
-        task = tasks.get_capabilities.apply_async((settings.CDAS_HOST,
-            settings.CDAS_REQUEST_PORT,
-            settings.CDAS_RESPONSE_PORT))
-
-        result = task.get()
-
-        return result
-    elif wps_request == 'execute':
-        try:
-            data_inputs = request.GET['datainputs']
-        except KeyError:
-            raise MalformedWPSRequestError()
-
-        try:
-            identifier = request.GET['Identifier']
-        except KeyError:
-            raise MalformedWPSRequestError()
-
-        task = tasks.execute.apply_async((settings.CDAS_HOST,
-            settings.CDAS_REQUEST_PORT,
-            settings.CDAS_RESPONSE_PORT,
-            identifier,
-            data_inputs))
-
-        result = task.get()
-
-        return result
-
-    return http.HttpResponseForbidden() 
