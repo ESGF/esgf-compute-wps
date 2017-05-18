3b073d9e89dc47a8dc2fb753067b53324b1ef637
Jason Boutte <boutte.jason@gmail.com>
2017-05-17 22:46:31 -0700

    Fixes entrypoint file.

--- docker/cdas2/entrypoint.sh
+++ docker/cdas2/entrypoint.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-<<<<<<< HEAD
+set -e
+
 source bin/setup_runtime.sh
 
 export CDAS_JAR=${CDAS_HOME_DIR}/target/scala-2.10/cdas2_2.10-1.2.2-SNAPSHOT.jar
@@ -9,14 +10,5 @@ export APP_DEP_JARS=$(find ~/.ivy2 -name "*.jar" )
 export APP_DEP_CP=$(echo $APP_DEP_JARS | sed -e "s/ /:/g" ):${CONDA_LIB}
 
 java -cp $APP_DEP_CP:$CDAS_JAR nasa.nccs.cdas.portal.CDASApplication bind $CDAS_REQUEST_PORT $CDAS_RESPONSE_PORT ~/.cdas/cache/cdas.properties
-=======
-set -e
-
-echo 'spark.master=local' >> ${HOME}/.cdas/cache/cdas.properties
-
-source bin/setup_runtime.sh
-
-LD_LIBRARY_PATH=/opt/conda/lib sbt 'run bind 4356 4357'
->>>>>>> a03d58738bda0ecb751a443b04a2525c14153173
 
 exec "$@"
