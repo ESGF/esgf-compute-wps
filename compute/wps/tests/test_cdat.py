7b5ccf7fdafc7c70d78f56727c98acdd0e8fd245
Jason Boutte <boutte.jason@gmail.com>
2017-05-20 11:45:41 +0000

    Fixes unittesting.

--- compute/wps/tests/test_cdat.py
+++ compute/wps/tests/test_cdat.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python
 
 import os
+import shutil
 
 import cdms2
+import mock
 import numpy as np
 import requests
 from contextlib import closing
 from django import test
 
+from wps import settings
 from wps.processes import cdat
 
 def generate_time(start, stop, units):
@@ -28,146 +31,101 @@ def generate_variable(value, time, lat, lon, var_name, identifier):
 
     with closing(cdms2.open(file_path, 'w')) as f:
         data = np.array([[[value
-                           for _ in xrange(len(lat))]
-                          for _ in xrange(len(lon))]
+                           for _ in xrange(len(lon))]
+                          for _ in xrange(len(lat))]
                          for _ in xrange(len(time))])
 
-        f.write(data, axes=(time, lon, lat), id=var_name)
-
-    return [(identifier, {'uri':file_path,'id': '{}|{}'.format(var_name, identifier)})]
-
-def generate_grid(value, var_name, identifier):
-    file_name = '{}.nc'.format(identifier)
-
-    file_path = '{}/{}'.format(os.path.dirname(__file__), file_name)
-
-    grid, arg = value.split('~')
-
-    if grid == 'gaussian':
-        g = cdms2.createGaussianGrid(int(arg))
-
-        g.writeToFile(file_path)
+        f.write(data, axes=(time, lat, lon), id=var_name)
 
     return [(identifier, {'uri':file_path,'id': '{}|{}'.format(var_name, identifier)})]
 
 class TestCDAT(test.TestCase):
 
     def setUp(self):
+        settings.CACHE_PATH = os.path.join(os.path.dirname(__file__), 'cache')
+
+        self.cache = settings.CACHE_PATH
+
+        os.mkdir(settings.CACHE_PATH)
+
         time1 = generate_time(0, 365, 'days since 1990-1-1')
 
         lat_1 = cdms2.createUniformLatitudeAxis(-89.5, 180, 1)
 
         lon_1 = cdms2.createUniformLongitudeAxis(0.5, 360, 1)
 
+        self.d = {'d0': {'id': 'd0',
+                         'time': {'start': 100, 'end': 200, 'crs': 'indices'},
+                         'lat': {'start': 0, 'end': 90, 'crs': 'indices'},
+                         'lon': {'start': 180, 'end': 360, 'crs': 'indices'},
+                        },
+                  'd1': {'id': 'd1',
+                         'time': {'start': 100, 'end': 200, 'crs': 'values'},
+                         'latitude': {'start': -90, 'end': 0, 'crs': 'values'},
+                         'longitude': {'start': 180, 'end': 360, 'crs': 'values'},
+                        },
+                 }
+
         self.v = {}
 
         self.v.update(generate_variable(10, time1, lat_1, lon_1, 'tas', 'tas_10_365_180_360'))
-        self.v.update(generate_grid('gaussian~32', 'tas', 'weird_grid'))
 
-    def test_subset_bad_time_indices(self):
-        o = {'CDAT.subset':{'name':'CDAT.subset','domain':'d0','input':['tas_10_365_180_360']}}
+    def tearDown(self):
+        for v in self.v.values():
+            os.remove(v['uri'])
 
-        d = {'d0':{'id':'d0',
-                   'time':{'start':0,'end':500,'crs':'indices'}
-                  }}
-
-        r = cdat.subset(self.v, o, d, local=True)
-
-        with closing(cdms2.open(r['uri'], 'r')) as f:
-            tas = f['tas']
-
-            self.assertEqual(tas.shape, (365, 180, 360))
-
-    def test_subset_regrid_domain(self):
-        o = {'CDAT.subset':{'name':'CDAT.subset',
-                            'domain':'d0',
-                            'input':['tas_10_365_180_360'],
-                            'gridder':{'tool':'esmf','method':'linear','grid':'grid'}}}
-
-        d = {'d0':{'id':'d0',
-                   'time':{'start':100,'end':300,'crs':'indices'},
-                   'latitude':{'start':0,'end':90,'crs':'indices'},
-                   'longitude':{'start':180,'end':270,'crs':'indices'},
-                  },
-             'grid':{'id':'grid',
-                     'latitude':{'start':0,'end':32,'crs':'values'},
-                     'longitude':{'start':0,'end':64,'crs':'values'},
-                    },
+        shutil.rmtree(self.cache) 
+    
+    @mock.patch('wps.processes.process.CWTBaseTask.set_user_creds')
+    def test_subset_bad_input(self, mock):
+        o = {'CDAT.subset': {'name': 'CDAT.subset',
+                             'input': ['tas'],
+                             'domain': 'd0',
+                            },
             }
 
-        r = cdat.subset(self.v, o, d, local=True)
+        v = {'tas': {'uri': 'file:///no.nc',
+                     'id': 'tas|tas',
+                    }
+            }
 
-        with closing(cdms2.open(r['uri'], 'r')) as f:
-            tas = f['tas']
+        with self.assertRaises(Exception):
+            cdat.subset(v, o, self.d, local=True)
 
-            self.assertEqual(tas.shape, (200, 32, 64))
+    @mock.patch('wps.processes.process.CWTBaseTask.set_user_creds')
+    def test_subset_regrid(self, mock):
+        o = {'CDAT.subset': {'name': 'CDAT.subset',
+                             'input': ['tas_10_365_180_360'],
+                             'domain': 'd1',
+                             'gridder': {'tool': 'esmf', 'method': 'linear', 'grid': 'gaussian~32'}
+                            },
+            }
 
-    def test_subset_regrid_file(self):
-        o = {'CDAT.subset':{'name':'CDAT.subset',
-                            'domain':'d0',
-                            'input':['tas_10_365_180_360'],
-                            'gridder':{'tool':'esmf','method':'linear','grid':'weird_grid'}}}
+        result = cdat.subset(self.v, o, self.d, local=True)
 
-        d = {'d0':{'id':'d0',
-                   'time':{'start':100,'end':300,'crs':'indices'},
-                   'latitude':{'start':0,'end':90,'crs':'indices'},
-                   'longitude':{'start':180,'end':270,'crs':'indices'},
-                  }}
+        with cdms2.open(result['uri']) as f:
+            self.assertEqual(f['tas'].shape, (100, 32, 64))
 
-        r = cdat.subset(self.v, o, d, local=True)
+    @mock.patch('wps.processes.process.CWTBaseTask.set_user_creds')
+    def test_subset_values(self, mock):
+        o = {'CDAT.subset': {'name': 'CDAT.subset',
+                             'input': ['tas_10_365_180_360'],
+                             'domain': 'd1'},
+            }
 
-        with closing(cdms2.open(r['uri'], 'r')) as f:
-            tas = f['tas']
+        result = cdat.subset(self.v, o, self.d, local=True)
 
-            self.assertEqual(tas.shape, (200, 64, 32))
+        with cdms2.open(result['uri']) as f:
+            self.assertEqual(f['tas'].shape, (100, 90, 180))
 
-    def test_subset_regrid(self):
-        o = {'CDAT.subset':{'name':'CDAT.subset',
-                            'domain':'d0',
-                            'input':['tas_10_365_180_360'],
-                            'gridder':{'tool':'esmf','method':'linear','grid':'gaussian~32'}}}
+    @mock.patch('wps.processes.process.CWTBaseTask.set_user_creds')
+    def test_subset_indices(self, mock):
+        o = {'CDAT.subset': {'name': 'CDAT.subset',
+                             'input': ['tas_10_365_180_360'],
+                             'domain': 'd0'},
+            }
 
-        d = {'d0':{'id':'d0',
-                   'time':{'start':100,'end':300,'crs':'indices'},
-                   'latitude':{'start':0,'end':90,'crs':'indices'},
-                   'longitude':{'start':180,'end':270,'crs':'indices'},
-                  }}
+        result = cdat.subset(self.v, o, self.d, local=True)
 
-        r = cdat.subset(self.v, o, d, local=True)
-
-        with closing(cdms2.open(r['uri'], 'r')) as f:
-            tas = f['tas']
-
-            self.assertEqual(tas.shape, (200, 32, 64))
-
-    def test_subset_values(self):
-        o = {'CDAT.subset':{'name':'CDAT.subset','domain':'d0','input':['tas_10_365_180_360']}}
-
-        d = {'d0':{'id':'d0',
-                   'time':{'start':100,'end':300,'crs':'values'},
-                   'latitude':{'start':-90,'end':0,'crs':'values'},
-                   'longitude':{'start':180,'end':270,'crs':'values'},
-                  }}
-
-        r = cdat.subset(self.v, o, d, local=True)
-
-        with closing(cdms2.open(r['uri'], 'r')) as f:
-            tas = f['tas']
-
-            self.assertEqual(tas.shape, (201, 90, 90))
-
-    def test_subset_indices(self):
-        o = {'CDAT.subset':{'name':'CDAT.subset','domain':'d0','input':['tas_10_365_180_360']}}
-
-        d = {'d0':{'id':'d0',
-                   'time':{'start':100,'end':300,'crs':'indices'},
-                   'latitude':{'start':0,'end':90,'crs':'indices'},
-                   'longitude':{'start':180,'end':270,'crs':'indices'},
-                  }}
-
-        r = cdat.subset(self.v, o, d, local=True)
-
-        with closing(cdms2.open(r['uri'], 'r')) as f:
-            tas = f['tas']
-
-            self.assertEqual(tas.shape, (200, 90, 90))
+        with cdms2.open(result['uri']) as f:
+            self.assertEqual(f['tas'].shape, (100, 90, 180))
