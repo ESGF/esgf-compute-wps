1bb66984b6040fe4b720b5864d0e391000489a65
Jason Boutte <boutte.jason@gmail.com>
2017-07-25 20:44:58 +0000

    Fixes the default for collecting static files.

--- compute/compute/settings.py
+++ compute/compute/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'wps', 'assets')
+STATIC_ROOT = '/var/www/static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'wps', 'assets'),
