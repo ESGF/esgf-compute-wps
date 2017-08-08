4e586c600452d620eb1bbd0b60a8b72cdfb0d1a1
Jason Boutte <boutte.jason@gmail.com>
2017-08-07 18:54:48 -0700

    Adds ROI selection.

--- compute/wps/assets/app/configure.component.ts
+++ compute/wps/assets/app/configure.component.ts
@@ -8,6 +8,8 @@ import { Dimension } from './dimension.component';
 import * as d3 from './d3.bundle';
 import * as topojson from 'topojson';
 
+import { event } from 'd3-selection';
+
 class Config {
   process: string;
   variable: string;
@@ -27,6 +29,7 @@ export class ConfigureComponent implements OnInit  {
   variables: string[] = [];
   files: string[] = [];
   svg: any = null;
+  roi: any = null;
   dimensions: Dimension[] = [
     new Dimension('longitude', 'Degree', -180, 180, 1),
     new Dimension('latitude', 'Degree', 90, -90, 1)
@@ -46,6 +49,32 @@ export class ConfigureComponent implements OnInit  {
     this.loadMap();
   }
 
+  onDragStart() {
+    return () => {
+      const e = <d3.D3DragEvent<SVGRectElement, any, any>> event;
+
+      let coord = d3.mouse(this.svg.node());
+
+      this.roi.attr('x', coord[0])
+        .attr('y', coord[1]);
+
+      this.roi.attr('width', 0);
+      this.roi.attr('height', 0);
+    }
+  }
+
+  onDrag() {
+    return () => {
+      const e = <d3.D3DragEvent<SVGRectElement, any, any>> event;
+
+      let width = +this.roi.attr('width');
+      let height = +this.roi.attr('height');
+
+      this.roi.attr('width', width + e.dx);
+      this.roi.attr('height', height + e.dy);
+    }
+  }
+
   loadMap(): void {
     this.svg = d3.select('svg')
       .attr('width', 960)
@@ -63,6 +92,18 @@ export class ConfigureComponent implements OnInit  {
       .datum(graticule)
       .attr('class', 'graticule')
       .attr('d', path);
+
+    this.roi = this.svg.append('rect')
+      .attr('class', 'roi')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', 0)
+      .attr('height', 0);
+
+    this.svg.call(d3.drag()
+      .on('start', this.onDragStart())
+      .on('drag', this.onDrag())
+    );
 
     d3.json('/static/data/ne_50m_admin_0.json', (error: any, world: any) => {
       if (error) throw error;
--- compute/wps/assets/app/d3.bundle.ts
+++ compute/wps/assets/app/d3.bundle.ts
@@ -3,3 +3,5 @@ export * from 'd3-request';
 export * from 'd3-geo';
 export * from 'd3-scale';
 export * from 'd3-array';
+export * from 'd3-dispatch';
+export * from 'd3-drag';
