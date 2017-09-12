72bde24e203e2bac8a1668fbc288651950b15966
Jason Boutte <boutte.jason@gmail.com>
2017-09-12 10:29:58 -0700

    Fixes default static root.

--- compute/compute/settings.py
+++ compute/compute/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = '/var/www/static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
