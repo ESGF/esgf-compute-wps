8e2f4f4c982c08a4fba4ef95fc118620e7fa36cd
Jason Boutte <boutte.jason@gmail.com>
2017-09-12 13:45:20 -0700

    Removes postgres wait from wps container

--- docker/wps/entrypoint.sh
+++ docker/wps/entrypoint.sh
@@ -2,16 +2,6 @@
 
 python manage.py collectstatic --no-input
 
-if [ ! -z "$POSTGRES_HOST" ]
-then
-  until $(ping -c1 "$POSTGRES_HOST" 2> /dev/null && echo $?)
-  do
-    echo "Waiting for database at $POSTGRES_HOST"
-
-    sleep 4
-  done
-
-  python manage.py migrate
-fi
+python manage.py migrate
 
 exec $@
