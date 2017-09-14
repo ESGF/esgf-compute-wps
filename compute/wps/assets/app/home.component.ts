7075ed380597f1f4eb218fccadccf7474afee7fd
Jason Boutte <boutte.jason@gmail.com>
2017-09-14 05:27:24 +0000

    Updates home page

--- compute/wps/assets/app/home.component.ts
+++ compute/wps/assets/app/home.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   template: `
   <div class="container">
+    <h1 class="text-center">Welcome to LLNL's CWT WPS server</h1>
     <div class="row">
       <div class="col-md-12">
+        <br>
         <p>
-          Welcome to LLNL's CWT WPS server. 
-        </p>
-        <p>
-          To get started you must create an <a [routerLink]="['/wps/home/create']">account</a>.
+          To get started either create an <a [routerLink]="['/wps/home/create']">account</a> or login into using your <a [routerLink]="['/wps/home/login/openid']">IDP</a>.
         </p>
       </div>
     </div>
