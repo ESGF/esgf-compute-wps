9c45c1e907d5a4b6653998862d76537f955d9a2b
Jason Boutte <boutte.jason@gmail.com>
2017-09-30 08:24:40 +0000

    Adds django admin command to regenerate capabilties #55

---
+++ compute/wps/management/commands/capabilities.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from wps import node_manager
+
+
+class Command(BaseCommand):
+    help = 'Generates WPS capabilities document'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        manager = node_manager.NodeManager()
+
+        manager.generate_capabilities()
