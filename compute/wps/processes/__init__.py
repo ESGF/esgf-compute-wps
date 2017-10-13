fcf25cdbce5ce5298634fa575060ef25a600076f
Jason Boutte <boutte.jason@gmail.com>
2017-10-13 23:36:00 +0000

    Removes flags for creating netcdf3 files

--- compute/wps/processes/__init__.py
+++ compute/wps/processes/__init__.py
@@ -4,9 +4,9 @@ from __future__ import absolute_import
 
 import cdms2
 
-cdms2.setNetcdfShuffleFlag(0)
-cdms2.setNetcdfDeflateFlag(0)
-cdms2.setNetcdfDeflateLevelFlag(0)
+#cdms2.setNetcdfShuffleFlag(0)
+#cdms2.setNetcdfDeflateFlag(0)
+#cdms2.setNetcdfDeflateLevelFlag(0)
 
 from wps.processes.process import *
 from wps.processes.cdat import *
