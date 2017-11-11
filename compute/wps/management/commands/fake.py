633c3bdc9e59012ea9724f4657138a50f048a4af
Jason Boutte <boutte.jason@gmail.com>
2017-11-11 04:05:08 +0000

    Updates the fake data command

--- compute/wps/management/commands/fake.py
+++ compute/wps/management/commands/fake.py
@@ -8,130 +8,191 @@ from django.core.management.base import BaseCommand, CommandError
 from wps import models
 
 class Command(BaseCommand):
-    help = 'Adds fake data'
+    help = 'Adds fake data to database'
 
     def add_arguments(self, parser):
-        parser.add_argument('--users', type=int)
-        parser.add_argument('--files', type=int)
-        parser.add_argument('--usage', action='store_true')
-        parser.add_argument('--process', type=int)
-        parser.add_argument('--jobs', action='store_true')
-        parser.add_argument('--clear', action='store_true')
+        parser.add_argument('--servers', type=int, help='Create fake servers')
+        parser.add_argument('--processes', type=int, help='Create fake processes')
+        parser.add_argument('--users', type=int, help='Create fake users')
+        parser.add_argument('--files', type=int, help='Create fake files')
+        parser.add_argument('--caches', type=int, help='Create fake cache files')
+        parser.add_argument('--clear', action='store_true', help='Clear database')
 
-    def __get_required_option(self, key, options):
-        value = options[key]
-
-        if value == None:
-            raise CommandError('Option "{}" is required for action "{}"'.format(key, options['action']))
-
-        return value
-
-    def random_string_length(self, low, high):
-        return ''.join(random.choice(string.ascii_letters+string.digits) for _ in xrange(random.randint(low, high)))
-
-    def random_string(self, count):
+    def random_str(self, count):
         return ''.join(random.choice(string.ascii_letters+string.digits) for _ in xrange(count))
 
-    def random_file_name(self):
-        return self.random_string(64) + '.nc'
+    def random_email(self):
+        return '{}@{}.{}'.format(self.random_str(10), self.random_str(5), random.choice(['com', 'org', 'gov', 'net']))
 
-    def random_file_url(self):
-        return 'http://test.com/{}'.format(self.random_file_name())
+    def random_path(self):
+        return '/path/{}.nc'.format(self.random_str(32))
+
+    def random_url(self):
+        return 'https://{}.com/thredds/{}.nc'.format(self.random_str(16), self.random_str(32))
+
+    def clear(self):
+        total = 0
+
+        todo = [
+            models.User,
+            models.File,
+            models.Cache,
+            models.Server,
+            models.Process,
+        ]
+
+        for m in todo:
+            if m == models.User:
+                removed, _ = m.objects.filter(username__contains='fake_').delete()
+            else:
+                removed, _ = m.objects.all().delete()
+
+            total += removed
+
+        self.stdout.write(self.style.SUCCESS('Removed {} records'.format(total)))
+
+    def create_users(self, count):
+        for _ in xrange(count):
+            username = 'fake_{}'.format(self.random_str(10))
+
+            user = models.User.objects.create_user(username, self.random_email(), username)
+
+            models.Auth.objects.create(user=user)
+
+        self.stdout.write(self.style.SUCCESS('Created {} users'.format(count)))
+
+    def create_files(self, count):
+        variables = [self.random_str(4) for _ in xrange(count/4)]
+        hosts = [self.random_str(10) for _ in xrange(count/2)]
+
+        for _ in xrange(count):
+            filename = self.random_str(16)
+
+            file_hosts = random.sample(hosts, random.randint(1, len(hosts)))
+
+            for h in file_hosts:
+                url = 'https://{}.com/thredds/{}.nc'.format(h, filename)
+
+                models.File.objects.create(name=filename, host=h, variable=random.choice(variables), url=url, requested=random.randint(100, 1000000))
+
+        self.stdout.write(self.style.SUCCESS('Created {} files sample from {} hosts and {} variables'.format(count, len(hosts), len(variables))))
+
+    def create_caches(self, count):
+        for _ in xrange(count):
+            uid = self.random_str(64)
+
+            models.Cache.objects.create(uid=uid, url=self.random_path(), dimensions='0:365:1', size=random.randint(1000, 2000000))
+
+        self.stdout.write(self.style.SUCCESS('Created {} cached files'.format(count)))
+
+    def create_processes(self, count):
+        backends = ['backend1', 'backend2', 'backend3']
+
+        for _ in xrange(count):
+            process = models.Process.objects.create(identifier=self.random_str(16), backend=random.choice(backends))
+
+            models.ProcessUsage.objects.create(process=process,
+                                               executed=random.randint(0, 1000),
+                                               success=random.randint(0, 1000),
+                                               failed=random.randint(0, 1000),
+                                               retry=random.randint(0, 1000))
+
+        self.stdout.write(self.style.SUCCESS('Created {} processes'.format(count)))
+
+    def create_servers(self, count):
+        for _ in xrange(count):
+            models.Server.objects.create(host=self.random_str(16), capabilities='capabilities')
+
+        self.stdout.write(self.style.SUCCESS('Created {} servers'.format(count)))
+
+    def create_userfiles(self):
+        users = models.User.objects.all()
+
+        files = models.File.objects.all()
+
+        file_count = len(files)
+
+        for user in users:
+            sample = random.sample(files, random.randint(1, file_count))
+
+            for sample_file in sample:
+                models.UserFile.objects.create(user=user, file=sample_file, requested=random.randint(10, 1000))
+
+            self.stdout.write(self.style.SUCCESS('Associated {} files with user {}'.format(len(sample), user.id)))
+
+        self.stdout.write(self.style.SUCCESS('Finished creating user file associations'))
+
+    def create_userprocesses(self):
+        users = models.User.objects.all()
+
+        processes = models.Process.objects.all()
+
+        process_count = len(processes)
+
+        for user in users:
+            sample = random.sample(processes, random.randint(1, process_count))
+
+            for sample_process in sample:
+                models.UserProcess.objects.create(user=user, process=sample_process, requested=random.randint(10, 1000))
+
+            self.stdout.write(self.style.SUCCESS('Associated {} processes with user {}'.format(len(sample), user.id)))
+
+        self.stdout.write(self.style.SUCCESS('Finished creating user process associations'))
+
+    def create_jobs(self):
+        servers = models.Server.objects.all()
+
+        users = models.User.objects.all()
+
+        processes = models.Process.objects.all()
+
+        for user in users:
+            job_count = random.randint(2, 50)
+
+            for _ in xrange(job_count):
+                job = models.Job.objects.create(server=random.choice(servers),
+                                                user=user,
+                                                process=random.choice(processes))
+
+                job.accepted()
+
+                job.started()
+
+                steps = random.randint(0, 20)
+
+                for i in xrange(steps):
+                    job.update_status('Status step {}'.format(i), i*100/steps)
+
+                end_state = random.randint(0, 2)
+
+                if end_state == 0:
+                    job.succeeded(json.dumps({'uri': self.random_url()}))
+                elif end_state == 1:
+                    job.failed('Job Failed')
+                else:
+                    job.retry()
 
     def handle(self, *args, **options):
         if options['clear']:
-            models.Job.objects.all().delete()
+            self.clear()
 
-            models.User.objects.filter(username__contains='fake').delete()
+        if options['files']:
+            self.create_files(options['files'])
 
-            models.File.objects.all().delete()
+        if options['caches']:
+            self.create_caches(options['caches'])
 
-            models.Process.objects.all().delete()
+        if options['processes']:
+            self.create_processes(options['processes'])
 
-            self.stdout.write(self.style.SUCCESS('Successfully cleared all files and processes'))
-        else:
-            users = list(models.User.objects.all())
+        if options['servers']:
+            self.create_servers(options['servers'])
 
-            if options['users'] is not None:
-                for i in xrange(options['users']):
-                    username = 'fake{}'.format(i)
+        if options['users']:
+            self.create_users(options['users'])
 
-                    try:
-                        user = models.User.objects.create_user(username, 'fake1@test.com', username)
-                    except:
-                        continue
+            self.create_userfiles()
 
-                    models.Auth.objects.create(user=user)
+            self.create_userprocesses()
 
-                    users.append(user)
-
-                    self.stdout.write(self.style.SUCCESS('Created user "{username}" with password "{username}"'.format(username=username)))
-
-                self.stdout.write(self.style.SUCCESS('Successfully created "{}" fake users'.format(options['users'])))
-            else:
-                try:
-                    user = models.User.objects.create_user('fake1', 'fake1@test.com', 'fake1')
-                except:
-                    pass
-                else:
-                    models.Auth.objects.create(user=user)
-
-                    users.append(user)
-
-                    self.stdout.write(self.style.SUCCESS('Successfully created fake user "fake1" with password "fake1"'))
-
-            if options['jobs']:
-                server = models.Server.objects.get(host='default')
-
-                processes = models.Process.objects.all()
-
-                for user in users:
-                    for _ in xrange(random.randint(20, 50)):
-                        job = models.Job.objects.create(server=server, user=user, process=random.choice(processes))
-
-                        job.accepted()
-
-                        job.started()
-
-                        for i in xrange(random.randint(0, 5)):
-                            job.update_status('Status update {}'.format(i), 10)
-
-                        if random.random() > 0.5:
-                            job.failed('Some random failure')
-                        else:
-                            var = cwt.Variable('http://data-server.com/data/file.nc', 'tas')
-
-                            job.succeeded(json.dumps(var.parameterize()))
-
-                self.stdout.write(self.style.SUCCESS('Successfully created fake jobs'))
-
-            if options['files'] is not None:
-                var_name = self.random_string_length(3, 5)
-
-                for _ in xrange(options['files']):
-                    # Some entries will have different urls but same variable name
-                    if random.random() > 0.5:
-                        var_name = self.random_string_length(3, 5)
-
-                    var = cwt.Variable(self.random_file_url(), var_name)
-
-                    user = random.choice(users)
-
-                    # randomize how many times a user has accessed a file
-                    for _ in xrange(random.randint(5, 10)):
-                        models.File.track(user, var)
-
-                self.stdout.write(self.style.SUCCESS('Successfully created "{}" fake files'.format(options['files'])))
-
-            if options['process'] is not None:
-                for _ in xrange(options['process']):
-                    models.Process.objects.create(identifier=self.random_string(10), backend=self.random_string(10))
-
-                self.stdout.write(self.style.SUCCESS('Successfully created "{}" fake processes'.format(options['process'])))
-
-            if options['usage']:
-                for user in users:
-                    for p in models.Process.objects.all():
-                        models.UserProcess.objects.create(user=user, process=p, requested=random.randint(10, 60))
-
-                self.stdout.write(self.style.SUCCESS('Successfully created fake process usage'))
+            self.create_jobs()
