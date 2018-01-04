a7cb60b5b6b5a131faba41e1462861eec3d15631
Jason Boutte <boutte.jason@gmail.com>
2018-01-04 10:15:04 -0800

    Updates docker build script

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,130 +1,52 @@
 #! /bin/bash
 
-function usage {
-  echo -e "Available arguments:"
-  echo -e "\t--all \t\tBuild all images"
-  echo -e "\t--wps \t\tBuilds WPS image"
-  echo -e "\t--celery: \tBuilds Celery image"
-  echo -e "\t--thredds: \tBuilds Thredds image"
-  echo -e "\t--edas: \tBuilds EDAS2 image"
-  echo -e "\t--push: \tPush built images"
-  echo -e "\t--no-cache: \tBuild without using cached images"
-  echo -e "\t--test: \tRuns E2E test"
-}
-
-if [[ $# -eq 0 ]]
-then
-  usage
-  exit
-fi
+no_cache=""
+images=""
+git_tag_arg=""
 
 while [[ $# -gt 0 ]]
 do
-  key="$1"
+  arg="$1"
 
   shift
 
-  case $key in
-    --help|-h)
-      usage
-      exit
-      ;;
-    --test)
-      TEST=1
-      ;;
-    --all)
-      WPS=1
-      CELERY=1
-      THREDDS=1
-      EDAS=1
-      ;;
-    --wps)
-      WPS=1
-      ;;
-    --celery)
-      CELERY=1
-      ;;
-    --thredds)
-      THREDDS=1
-      ;;
-    --edas)
-      EDAS=1
-      if [[ "$1" == "--tag" ]]
-      then
-        shift
-        EDAS_TAG="--build-arg TAG=$1"
-        shift
-      else
-        EDAS_TAG=""
-      fi
-      ;;
-    --push)
-      PUSH=1
-      ;;
+  case ${arg} in
     --no-cache)
-      NOCACHE="--no-cache"
+      no_cache="--no-cache"
+      ;; 
+    --images)
+      images="$1"
+      shift
       ;;
-      *)
-      usage
-      exit
+    --images-all)
+      images="common,wps,celery,thredds,edas"
+      ;;
+    --git-tag)
+      git_tag_arg="--build-arg TAG=$1"
+      shift
       ;;
   esac
 done
 
-repo="jasonb87"
-
-echo "Building images and pushing to repo ${repo}"
-
-docker -v
-
-function build {
-  echo $*
-
-  docker build ${NOCACHE} -t ${repo}/${1}:latest -f ${2}/Dockerfile ${3} ${2}
-
-  if [[ -n $PUSH ]]
-  then
-    docker push ${repo}/${1}:latest
-  fi
+function check_command {
+  command -v $1 >/dev/null 2>&1 && $2 || $3
 }
 
-build cwt_common ../docker/common/
+# check that docker exists
+check_command docker "docker --version" "exit 1"
+check_command docker-compose "docker-compose --version" "exit 1"
 
-if [[ -n $WPS ]]
-then
-  echo "Building WPS image"
+script_dir="$(cd "$(dirname $0)" && pwd)"
 
-  build cwt_wps ../docker/wps
-fi
+# make sure we're in the git repo
+cd $script_dir
 
-if [[ -n $CELERY ]]
-then
-  echo "Building Celery image"
+git_dir="$(git rev-parse --show-toplevel)"
 
-  build cwt_celery ../docker/celery
-fi
+docker_dir="${git_dir}/docker"
 
-if [[ -n $THREDDS ]]
-then
-  echo "Building Thredds image"
-
-  build cwt_thredds ../docker/thredds
-fi
-
-if [[ -n $EDAS ]]
-then
-  echo "Building EDAS image $EDAS_TAG"
-
-  build cwt_edas ../docker/edas "$EDAS_TAG"
-fi
-
-if [[ -n $TEST ]]
-then
-  echo "Testing images"
-
-  docker-compose -f ../docker/docker-compose.yml down -v --remove-orphans
-
-  docker-compose -f ../docker/docker-compose.yml up -d
-
-  docker-compose -f ../docker/docker-compose.yml ps
-fi
+IFS="," read -ra images <<< "$images"
+for img in "${images[@]}"
+do
+  docker build -t jasonb/cwt_${img} ${no_cache} ${build_tag_arg} "${docker_dir}/${img}"
+done
