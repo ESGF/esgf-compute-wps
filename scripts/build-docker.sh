c81d0f94a92b278df202b89d39a9ec378470d6c5
Jason Boutte <boutte.jason@gmail.com>
2018-01-05 14:55:57 -0800

    Updates docker build script to expose docker repo name and make docker-compose test optional

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,8 +1,28 @@
 #! /bin/bash
 
+function help {
+  echo -e "Usage: $0 ...\n"
+  echo -e "--no-cache: \t\tDisable caching when building docker images"
+  echo -e "--images: \t\tCommon separated list of images to build"
+  echo -e "--images-all: \t\tBuild all images"
+  echo -e "--git-tag: \t\tGithub branch to build, this will be passed to all "
+  echo -e "\t\t\timages. If you need separate branches launch multiple commands"
+  echo -e "--docker-repo: \t\tThe docker repo to tag the image ${docker_repo}/image-name"
+  echo -e "--test-compose: \t\tWill run docker-compose after building"
+}
+
 no_cache=""
 images=""
 git_tag_arg=""
+docker_repo=""
+test_compose=0
+
+if [[ $# -eq 0 ]]
+then
+  help
+
+  exit
+fi
 
 while [[ $# -gt 0 ]]
 do
@@ -24,6 +44,13 @@ do
     --git-tag)
       git_tag_arg="--build-arg TAG=$1"
       shift
+      ;;
+    --docker-repo)
+      docker_repo="$1/"
+      shift
+      ;;
+    --test-compose)
+      test_compose=1
       ;;
   esac
 done
@@ -48,5 +75,29 @@ docker_dir="${git_dir}/docker"
 IFS="," read -ra images <<< "$images"
 for img in "${images[@]}"
 do
-  docker build -t jasonb/cwt_${img} ${no_cache} ${build_tag_arg} "${docker_dir}/${img}"
+  docker build -t ${docker_repo}cwt_${img} ${no_cache} ${build_tag_arg} "${docker_dir}/${img}"
 done
+
+docker_file="docker-compose.dev.yml"
+
+running="$(docker-compose -p test -f ${docker_dir}/${docker_file} ps -q 2>/dev/null | wc -l)"
+
+if [[ "$test_compose" -eq 1 ]]
+then
+  if [[ "$running" -gt 6 ]]
+  then
+    docker-compose -p tst -f ${docker_dir}/${docker_file} down -v
+
+    sleep 10
+  fi
+
+  docker-compose -p test -f ${docker_dir}/${docker_file} up -d
+
+  sleep 10
+
+  docker-compose -p test -f ${docker_dir}/${docker_file} ps
+
+  sleep 10
+
+  docker-compose -p test -f ${docker_dir}/${docker_file} down -v
+fi
