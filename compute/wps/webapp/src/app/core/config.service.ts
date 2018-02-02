e62320887452744c1601a0c8f27406d7f064b05e
Jason Boutte <boutte.jason@gmail.com>
2018-02-02 13:26:34 -0800

    Fixes default cog url

--- compute/wps/webapp/src/app/core/config.service.ts
+++ compute/wps/webapp/src/app/core/config.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-  esgfURL: string = 'https://esgf.llnl.gov';
-  cogURL: string = 'http://10.5.5.5:8010/search/testproject';
+  esgfURL = 'https://esgf.llnl.gov';
+  cogURL = 'https://esgf-node.llnl.gov/search/esgf-llnl';
 }
