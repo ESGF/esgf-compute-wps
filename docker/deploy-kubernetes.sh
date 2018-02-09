190305e87da250b5c35ce5352a71f79f788a318b
Jason Boutte <boutte.jason@gmail.com>
2018-02-08 18:13:01 -0800

    Fixes traefik to provide ssl termination

--- docker/deploy-kubernetes.sh
+++ docker/deploy-kubernetes.sh
@@ -9,6 +9,8 @@ function usage {
   echo "  --client: OAuth2 Client ID"
   echo "  --secret: OAuth2 Secret"
   echo "  --postgres: PostgreSQL password"
+  echo "  --sslcert: SSL Certificate"
+  echo "  --sslkey: SSL Key"
 }
 
 [[ $# -eq 0 ]] && usage && exit 1
@@ -31,6 +33,14 @@ do
       postgres_pass=$1
       shift
       ;;
+    --sslcert)
+      ssl_cert=$1
+      shift
+      ;;
+    --sslkey)
+      ssl_key=$1
+      shift
+      ;;
     *)
       usage
       exit 1
@@ -42,11 +52,15 @@ done
 [ -z "${oauth_client}" ] && echo "Missing required --client argument" && exit 1
 [ -z "${oauth_secret}" ] && echo "Missing required --secret argument" && exit 1
 [ -z "${postgres_pass}" ] && echo "Missing required --postgres argument" && exit 1
+[ -z "${ssl_cert}" ] && echo "Missing required --sslcert argument" && exit 1
+[ -z "${ssl_key}" ] && echo "Missing required --sslkey argument" && exit 1
 
 echo -e "${GREEN}Generating kubectl configs and secrets${CLEAR}"
 
 kubectl create configmap app-config --from-env-file=wps/app.properties
 kubectl create configmap django-config --from-file=wps/django.properties
+kubectl create configmap traefik-config --from-file=kubernetes/traefik.toml
+kubectl create secret generic ssl-secret --from-file=${ssl_cert} --from-file=${ssl_key}
 kubectl create secret generic app-secret --from-literal=oauth_client=${oauth_client} --from-literal=oauth_secret=${oauth_secret} --from-literal=postgres_password=${postgres_pass}
 
 kubectl create --filename=kubernetes/
