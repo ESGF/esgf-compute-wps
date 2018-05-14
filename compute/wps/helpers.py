ab1318eabdec99f393100854aced51988ecd660c
Jason Boutte <boutte.jason@gmail.com>
2018-05-14 19:49:13 +0000

    Adds helper module including methods to make slices JSON serializable

---
+++ compute/wps/helpers.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+def json_dumps_default(x):
+    if isinstance(x, slice):
+        return {
+            'slice': {
+                'start': x.start,
+                'stop': x.stop,
+                'step': x.step,
+            }
+        }
+    else:
+        raise TypeError()
+
+def json_loads_object_hook(x):
+    if 'slice' in x:
+        data = x['slice']
+
+        return slice(data['start'], data['stop'], data['step'])
