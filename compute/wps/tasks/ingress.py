b3f18554874adcdcc20eba17a9c6fefbcd196053
Jason Boutte <boutte.jason@gmail.com>
2018-05-14 23:46:52 +0000

    Updates preprocessing

--- compute/wps/tasks/ingress.py
+++ compute/wps/tasks/ingress.py
@@ -2,19 +2,25 @@
 
 import cdms2
 import cwt
+import json
 import requests
 from django.conf import settings
+from celery.utils import log
 
+from wps import helpers
 from wps.tasks import base
+from wps.tasks import process
 
 __ALL__ = [
     'preprocess',
     'ingress',
 ]
 
+logger = log.get_task_logger('wps.tasks.ingress')
+
 @base.cwt_shared_task()
 def preprocess(self, parent_variables, variables, domains, operation, user_id, job_id):
-    self.PUBLISH = base.ALL
+    self.PUBLISH = base.FAILURE | base.RETRY
 
     _, _, o = self.load(parent_variables, variables, domains, operation)
 
@@ -24,21 +30,36 @@ def preprocess(self, parent_variables, variables, domains, operation, user_id, j
 
     chunk_map = proc.generate_chunk_map(o)
 
+    o.domain = None
+
+    if 'domain' in o.parameters:
+        del o.parameters['domain']
+
+    o.inputs = []
+
     data = {
-        'chunk_map': chunk_map,
-        'operation': operation,
+        'chunk_map': json.dumps(chunk_map, default=helpers.json_dumps_default),
+        'operation': json.dumps(o.parameterize()),
         'user_id': user_id,
         'job_id': job_id
     }
 
-    response = requests.post(settings.WPS_INGRESS_URL, data)
+    session = requests.Session()
+
+    response = session.get(settings.WPS_ENDPOINT, verify=False)
+
+    csrf_token = session.cookies.get('csrftoken')
+
+    headers = { 'X-CSRFToken': csrf_token }
+
+    response = session.post(settings.WPS_INGRESS_URL, data, headers=headers, verify=False)
 
     if not response.ok:
-        raise base.WPSError('Failed to ingress data')
+        raise base.WPSError('Failed to ingress data status code {code}', code=response.status_code)
 
 @base.cwt_shared_task()
 def ingress(self, input_url, var_name, domain, output_uri):
-    self.PUBLISH = base.ALL
+    self.PUBLISH = base.FAILURE | base.RETRY
 
     temporal = domain['temporal']
 
