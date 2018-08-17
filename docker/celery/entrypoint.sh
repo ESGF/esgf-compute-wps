b3ef026b8a5ffb0aad3c1ac81d3b608e6aed0bab
Jason Boutte <boutte.jason@gmail.com>
2018-08-17 22:54:29 +0000

    Adds CWT_METRICS to helm charts #145

--- docker/celery/entrypoint.sh
+++ docker/celery/entrypoint.sh
@@ -15,21 +15,29 @@ if [[ ${celery_ret} -ne 0 ]]; then
   return ${celery_ret}
 fi
 
-exec python wps/metrics.py &
+if [[ -n "${CWT_METRICS}" ]]; then
+  exec python wps/metrics.py &
 
-metrics_ret=${?}
-metrics_pid=${!}
+  metrics_ret=${?}
+  metrics_pid=${!}
 
-if [[ ${metrics_ret} -ne 0 ]]; then
-  echo "Metrics failed to start"
+  if [[ ${metrics_ret} -ne 0 ]]; then
+    echo "Metrics failed to start"
 
-  return ${metrics_ret}
+    return ${metrics_ret}
+  fi
 fi
 
 while sleep 60; do
-  if [[ $(kill -0 ${celery_pid}) -ne 0 ]] || [[ $(kill -0 ${metrics_pid}) -ne 0 ]]; then
-    echo "Child process died"
+  if [[ $(kill -0 ${celery_pid}) -ne 0 ]]; then
+    echo "Celery process died"
 
-    exit 1 
+    exit 1
+  fi
+
+  if [[ -n "${CWT_METRICS}" ]] && [[ $(kill -0 ${metrics_pid}) -ne 0 ]]; then
+    echo "Metrics process died"
+
+    exit 1
   fi
 done
