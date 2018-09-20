476d67a570bcd58abca5c87c2a581e22e64a36ea
Jason Boutte <boutte.jason@gmail.com>
2018-09-20 04:42:22 +0000

    Fixes jenkinsfile and failing tests

--- compute/wps/tests/test_tasks_preprocess.py
+++ compute/wps/tests/test_tasks_preprocess.py
@@ -495,6 +495,8 @@ class PreprocessTestCase(test.TestCase):
             'time': slice(0, 2000),
         }
 
+        self.maxDiff = None
+
         type(mock_entries.return_value).dimensions = mock.PropertyMock(return_value=helpers.encoder(data))
         type(mock_entries.return_value).local_path = mock.PropertyMock(return_value='./file1.nc')
 
@@ -523,9 +525,9 @@ class PreprocessTestCase(test.TestCase):
                 'cached': {
                     'path': './file1.nc',
                     'mapped': {
-                        'lat': slice(80, 180),
-                        'lon': slice(10, 60),
-                        'time': slice(0, 1500),
+                        'lat': slice(80, 180, 1),
+                        'lon': slice(10, 60, 1),
+                        'time': slice(0, 1500, 1),
                     }
                 },
                 'mapped': {
