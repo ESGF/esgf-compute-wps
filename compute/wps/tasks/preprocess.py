6452741fd094d5ab29ed23e7a2874160f8b137fd
Jason Boutte <boutte.jason@gmail.com>
2018-11-16 11:00:08 +0000

    Fixes check memory requirements for operations

--- compute/wps/tasks/preprocess.py
+++ compute/wps/tasks/preprocess.py
@@ -80,8 +80,6 @@ def generate_chunks(self, attrs, operation, uri, process_axes, job_id):
 
     self.update(job, 'Generating chunks for {!r}', uri)
 
-    logger.info('Process axes %r', process_axes)
-
     try:
         mapped = attrs[uri]['cached']['mapped']
     except (TypeError, KeyError):
@@ -92,10 +90,7 @@ def generate_chunks(self, attrs, operation, uri, process_axes, job_id):
 
         return attrs
 
-    # Determine the axis to generate chunks for, defaults to time axis
-    if operation.identifier == 'CDAT.average' and len(process_axes) > 1:
-        chunked_axis = None
-    elif process_axes is None or ('time' in mapped and 'time' not in process_axes):
+    if process_axes is None or ('time' in mapped and 'time' not in process_axes):
         chunked_axis = 'time'
     else:
         process_axes = set(process_axes)
@@ -106,34 +101,41 @@ def generate_chunks(self, attrs, operation, uri, process_axes, job_id):
 
         chunked_axis = list(candidates)[0]
 
+    logger.info('Chunking axis %r', chunked_axis)
+
     axis_size = dict((x, (y.stop-y.start)/y.step) for x, y in mapped.iteritems())
 
-    chunk_axis_size = dict(x for x in axis_size.items() if chunked_axis is None
-                           or x[0] != chunked_axis)
+    logger.info('Axis size %r', axis_size)
 
-    logger.info('Worker memory size %r', settings.WORKER_MEMORY)
+    chunk_size = reduce(lambda x, y: x * y, [z[1] for z in axis_size.items()
+                                              if chunked_axis is None or z[0] !=
+                                              chunked_axis])*4
 
-    est_total_size = reduce(lambda x, y: x*y, axis_size.values())*4
+    logger.info('Chunk size %r', chunk_size)
 
-    logger.info('Estimated size in memory %r', est_total_size)
+    total_size = reduce(lambda x, y: x * y, axis_size.values())*4
 
-    est_chunk_size = reduce(lambda x, y: x*y, chunk_axis_size.values())*4
+    logger.info('Total size %r', total_size)
 
-    logger.info('Estimated chunk size in memory %r', est_chunk_size)
+    # Covers special case where average cannot be parallelized and data might
+    # not fit in memory.
+    if (operation.identifier == 'CDAT.average' and
+        'time' not in process_axes and
+        total_size >= settings.WORKER_MEMORY):
+        raise WPSError('Data is too large for workers memory try reducing the'
+                       'size by subsetting')
 
-    chunks_total = est_total_size/est_chunk_size
+    chunks_per_worker = int(round(settings.WORKER_MEMORY / chunk_size))
 
-    logger.info('Chunks total %r', chunks_total)
-
-    chunks_per_worker = round((settings.WORKER_MEMORY*0.50)/est_chunk_size)
+    if chunks_per_worker == 0:
+        raise WPSError('Chunk size of %r bytes is too large for workers memory'
+                       'try reducing the size by subsetting', est_chunk_size)
 
     logger.info('Chunks per worker %r', chunks_per_worker)
 
-    if chunks_per_worker == 0:
-        raise WPSError('Chunk size of %r bytes is too large for workers memory try reducing the size by subsetting', 
-                       est_chunk_size)
+    partition_size = min(axis_size[chunked_axis], chunks_per_worker)
 
-    partition_size = int(min(chunks_per_worker, chunks_total))
+    logger.info('Using partition size %r', partition_size)
 
     chunks = []
 
