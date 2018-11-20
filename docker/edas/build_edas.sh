faa0e94c90761d4e5208d78d855318aa98ae8b6b
Jason Boutte <boutte.jason@gmail.com>
2018-11-19 19:50:34 -0800

    Finish renaming edas to edask

--- docker/edas/build_edas.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cd edas/
-
-source bin/setup_runtime.sh
-
-sbt package
-
-sbt package
--- docker/edas/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cd /edas/
-
-source activate edas
-
-source bin/setup_runtime.sh
-
-exec sbt "run 5670 5671 /root/.edas/cache/edas.properties"
