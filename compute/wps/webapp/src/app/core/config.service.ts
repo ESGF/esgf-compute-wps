2499625cae6dcf6a453ef9df39cb5457933e2ca9
Jason Boutte <boutte.jason@gmail.com>
2018-12-20 08:21:52 +0000

    Cleans up the config service

--- compute/wps/webapp/src/app/core/config.service.ts
+++ compute/wps/webapp/src/app/core/config.service.ts
@@ -16,27 +16,19 @@ export class ConfigService {
   profilePath = `${this.basePath}/user/profile`;
   userJobPath = `${this.basePath}/user/jobs`;
   configurePath = `${this.basePath}/configure`;
-  adminPath = `${this.basePath}/admin`;
 
   // WPS API paths
   wpsPath = '/wps/';
   jobsPath = '/wps/jobs/';
   generatePath = '/wps/generate/';
-  processesPath = '/wps/processes/';
   searchPath = '/wps/search/';
   searchVariablePath = '/wps/search/variable/';
-  adminStatsPath = '/wps/admin/stats/';
  
   // Auth API paths
-  authLoginPath = '/auth/login/';
   authLoginOpenIDPath = '/auth/login/openid/';
   authLoginMPCPath = '/auth/login/mpc/';
   authLoginOAuth2Path = '/auth/login/oauth2/';
   authLogoutPath = '/auth/logout/';
-  authResetPath = '/auth/reset/';
-  authForgotPasswordPath = '/auth/forgot/password/';
-  authForgotUsernamePath = '/auth/forgot/username/';
-  authCreatePath = '/auth/create/';
   authUpdatePath = '/auth/update/';
   authUserPath = '/auth/user/';
   authUserCertPath = '/auth/user/cert/';
