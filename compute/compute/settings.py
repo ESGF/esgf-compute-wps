3354cd3a410ac4dbe1ea099a65bbbcadbd289e5f
Jason Boutte <boutte.jason@gmail.com>
2018-12-20 15:57:48 +0000

    Combines multiple template definitions

--- compute/compute/settings.py
+++ compute/compute/settings.py
@@ -230,22 +230,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'compute.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'compute.wsgi.application'
 
 # Database
@@ -272,7 +256,16 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'wps', 'webapp', 'src'),
-        ]
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     }
 ]
 
