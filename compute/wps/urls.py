307654023d76115a7e100ef5f7580235ca65248f
Jason Boutte <boutte.jason@gmail.com>
2019-03-07 18:29:13 +0000

    Cleans up the authentication/authorization urls

--- compute/wps/urls.py
+++ compute/wps/urls.py
@@ -5,21 +5,6 @@ from django.contrib import admin
 
 import views
 
-auth_urlpatterns = [
-    url(r'^update/$', views.update),
-    url(r'^authorization/?$', views.authorization),
-    url(r'^user/$', views.user_details),
-    url(r'^user/cert/$', views.user_cert),
-    url(r'^user/stats/$', views.user_stats),
-    url(r'^user/regenerate/$', views.regenerate),
-    url(r'^login/openid/$', views.user_login_openid),
-    url(r'^logout/$', views.user_logout),
-    url(r'^login/oauth2/$', views.login_oauth2),
-    url(r'^login/mpc/$', views.login_mpc),
-    url(r'^callback$', views.oauth2_callback),
-    url(r'^callback/openid/$', views.user_login_openid_callback),
-]
-
 api_urlpatterns = [
     url(r'^ping/?', views.ping),
     url(r'^armstrong/', include('grappelli.urls')),
@@ -32,7 +17,20 @@ api_urlpatterns = [
     url(r'^jobs/(?P<job_id>[0-9]*)/$', views.job),
     url(r'^metrics/?$', views.metrics_view),
     url(r'^combine/?$', views.combine),
-    url(r'^auth/', include(auth_urlpatterns)),
+
+    # Authentication and authorization
+    url(r'^user/$', views.user_details),
+    url(r'^user/authorization/$', views.authorization),
+    url(r'^user/cert/$', views.user_cert),
+    url(r'^user/regenerate/$', views.regenerate),
+    url(r'^user/stats/$', views.user_stats),
+    url(r'^user/update/$', views.update),
+    url(r'^openid/login/$', views.user_login_openid),
+    url(r'^openid/logout/$', views.user_logout),
+    url(r'^openid/callback/$', views.user_login_openid_callback),
+    url(r'^oauth2/$', views.login_oauth2),
+    url(r'^oauth2/callback/$', views.oauth2_callback),
+    url(r'^mpc/$', views.login_mpc),
 ]
 
 urlpatterns = [
