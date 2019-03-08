ba285d934bb0c0591bd1da9da3a5541261af122e
Jason Boutte <boutte.jason@gmail.com>
2019-03-08 22:28:17 +0000

    Adds owner permissions for all job views

--- compute/wps/views/job.py
+++ compute/wps/views/job.py
@@ -1,10 +1,15 @@
 #! /usr/bin/env python
 
 from rest_framework import mixins
+from rest_framework import permissions
 from rest_framework import viewsets
 
 from wps import models
 from wps import serializers
+
+class OwnerPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
 
 class StatusViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Status.objects.all()
@@ -16,3 +21,4 @@ class JobViewSet(mixins.ListModelMixin,
                  viewsets.GenericViewSet):
     queryset = models.Job.objects.all()
     serializer_class = serializers.JobSerializer
+    permissions_classes = (OwnerPermission,)
