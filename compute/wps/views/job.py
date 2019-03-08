edd34d37845d1156f86aa95d5a26d6579b9a7417
Jason Boutte <boutte.jason@gmail.com>
2019-03-08 22:15:51 +0000

    Adds destroy view for jobs

--- compute/wps/views/job.py
+++ compute/wps/views/job.py
@@ -1,25 +1,18 @@
 #! /usr/bin/env python
 
-import datetime
-
-from django import http
-from django.db.models import Max
-from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.decorators.http import require_http_methods
+from rest_framework import mixins
 from rest_framework import viewsets
 
-from . import common
 from wps import models
 from wps import serializers
-
-logger = common.logger
-
-SESSION_TIME_FMT = '%Y%m%d%H%M%S'
 
 class StatusViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Status.objects.all()
     serializer_class = serializers.StatusSerializer
 
-class JobViewSet(viewsets.ReadOnlyModelViewSet):
+class JobViewSet(mixins.ListModelMixin, 
+                 mixins.RetrieveModelMixin, 
+                 mixins.DestroyModelMixin, 
+                 viewsets.GenericViewSet):
     queryset = models.Job.objects.all()
     serializer_class = serializers.JobSerializer
