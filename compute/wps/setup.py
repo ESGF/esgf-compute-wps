7d9e806407ec99063dde690ecf7a72e59813d653
Jason Boutte <boutte.jason@gmail.com>
2019-03-16 06:21:23 +0000

    Moves wps django project into a package

---
+++ compute/wps/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='esgf-compute-wps',
+    version='devel',
+    author='Jason Boutte',
+    author_email='boutte3@llnl.gov',
+    description='WPS Django Application',
+    url='https://github.com/ESGF/esgf-compute-wps',
+    packages=setuptools.find_packages(),
+)
