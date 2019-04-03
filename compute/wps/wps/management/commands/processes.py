39031865554a16a12bba51112de0c829150adaf9
Jason Boutte <boutte.jason@gmail.com>
2019-04-03 20:40:24 +0000

    Fixes how we register processes, phasing out the idea of backends

--- compute/wps/wps/management/commands/processes.py
+++ compute/wps/wps/management/commands/processes.py
@@ -1,16 +1,19 @@
 import json
 
-from django.db import IntegrityError
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+from django.db import IntegrityError
 
-from wps import backends
 from wps import models
+from wps.tasks import REGISTRY
+
 
 class Command(BaseCommand):
     help = 'Register processes'
 
     def handle(self, *args, **options):
+        print(options)
         verbose = options['verbosity'] > 1
 
         # Get the default/local server
@@ -19,35 +22,28 @@ class Command(BaseCommand):
         except models.Server.DoesNotExist:
             raise CommandError('Default server does not exist')
 
-        for name, backend in list(backends.Backend.registry.items()):
-            setting_name = 'WPS_{}_ENABLED'.format(name.upper())
+        for name, process in list(REGISTRY.items()):
+            setting_name = 'WPS_{!s}_ENABLED'.format(process.BACKEND.upper())
 
-            # Default to false just incase the setting does not exist
             enabled = getattr(settings, setting_name, False)
 
-            self.stdout.write(self.style.SUCCESS('Backend "{}" enabled: {}'.format(name, enabled)))
-
             if enabled:
-                backend.populate_processes()
+                self.stdout.write(self.style.WARNING(
+                    'Backend {!r} enabled {!s} registering process {!r}'.format(process.BACKEND, enabled, name)))
 
-                for process in backend.processes:
-                    try:
-                        process = models.Process.objects.create(
-                            identifier=process['identifier'],
-                            backend=process['backend'],
-                            abstract=process['abstract'],
-                            metadata=json.dumps(process['metadata']),
-                            version='devel')
-                    except IntegrityError as e:
-                        self.stdout.write(self.style.NOTICE('  Error registering process {}'.format(process['identifier'])))
+                try:
+                    process = models.Process.objects.create(
+                        identifier=name,
+                        backend=process.BACKEND,
+                        abstract=process.ABSTRACT,
+                        metadata=json.dumps(process.METADATA),
+                        version='devel')
+                except IntegrityError as e:
+                    self.stdout.write(self.style.ERROR('Failed to register process {!r}'.format(name)))
 
-                        if verbose:
-                            self.stdout.write(self.style.ERROR('    {}'.format(e)))
-                    else:
-                        process.server_set.add(server)
+                    if verbose:
+                        self.stdout.write(self.style.ERROR('Exception: {!s}'.format(e)))
+                else:
+                    process.server_set.add(server)
 
-                        self.stdout.write(self.style.SUCCESS('  Registered process {}'.format(process.identifier)))
-            else:
-                result = models.Process.objects.filter(backend=name).delete()
-
-                self.stdout.write(self.style.SUCCESS('  Removed {} processes'.format(result[1].get('wps.Process', 0))))
+                    self.stdout.write(self.style.SUCCESS('Successfully registered process {!r}'.format(name)))
