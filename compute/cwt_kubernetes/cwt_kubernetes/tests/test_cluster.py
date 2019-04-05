921dd16de45a0d74687a80405e2ce7485aa2e10c
Jason Boutte <boutte.jason@gmail.com>
2019-04-05 19:54:28 +0000

    Updates Cluster to be more robust

--- compute/cwt_kubernetes/cwt_kubernetes/tests/test_cluster.py
+++ compute/cwt_kubernetes/cwt_kubernetes/tests/test_cluster.py
@@ -8,6 +8,59 @@ from cwt_kubernetes.cluster import Cluster
 
 class TestCluster(unittest.TestCase):
 
+    def create_pod(self, phase):
+        pod = mock.MagicMock()
+
+        pod.status.phase = phase
+
+        return pod
+
+    @mock.patch('cwt_kubernetes.cluster.config')
+    def test_cleanup_pods(self, config):
+        mock_pod = mock.MagicMock()
+
+        cluster = Cluster('default', mock_pod)
+
+        with mock.patch.object(cluster, 'delete_pod') as mock_delete:
+            mock_pod_list = mock.MagicMock()
+            mock_pod_list.items = [
+                self.create_pod('Completed'),
+                self.create_pod('Failed'),
+                self.create_pod('Running'),
+            ]
+
+            result = cluster.cleanup_pods(mock_pod_list)
+
+            mock_delete.assert_any_call(mock_pod_list.items[0])
+            mock_delete.assert_any_call(mock_pod_list.items[1])
+
+            self.assertEqual(len(result), 1)
+
+    @mock.patch('cwt_kubernetes.cluster.config')
+    def test_delete_pod(self, config):
+        mock_pod = mock.MagicMock()
+
+        cluster = Cluster('default', mock_pod)
+
+        with mock.patch.object(cluster, 'core_api') as mock_core:
+            mock_pod_running = mock.MagicMock()
+            mock_pod_running.metadata.name = 'dask-worker'
+
+            cluster.delete_pod(mock_pod_running)
+
+            mock_core.delete_namespaced_pod.assert_called_with('dask-worker', 'default')
+
+    @mock.patch('cwt_kubernetes.cluster.config')
+    def test_get_pods(self, config):
+        mock_pod = mock.MagicMock()
+
+        cluster = Cluster('default', mock_pod)
+
+        with mock.patch.object(cluster, 'core_api') as mock_core:
+            cluster.get_pods()
+
+            mock_core.list_namespaced_pod.assert_called_with('default', label_selector='app=dask,component=dask-worker')
+
     @mock.patch('cwt_kubernetes.cluster.config')
     def test_scale_down_exception(self, config):
         mock_pod = mock.MagicMock()
@@ -34,7 +87,7 @@ class TestCluster(unittest.TestCase):
             mock_pod3,
         ])
 
-        with mock.patch.object(cluster, 'core') as mock_core:
+        with mock.patch.object(cluster, 'core_api') as mock_core:
             mock_core.list_namespaced_pod.return_value = namespaced_pods
 
             mock_core.delete_namespaced_pod.side_effect = [
@@ -44,7 +97,7 @@ class TestCluster(unittest.TestCase):
 
             cluster.scale_down(['192.168.0.4', '192.168.0.2'])
 
-            mock_core.list_namespaced_pod.assert_called_with('default')
+            mock_core.list_namespaced_pod.assert_called_with('default', label_selector='app=dask,component=dask-worker')
 
             mock_core.delete_namespaced_pod.assert_any_call('dask-worker-test2', 'default')
 
@@ -56,7 +109,7 @@ class TestCluster(unittest.TestCase):
 
         cluster = Cluster('default', mock_pod)
 
-        with mock.patch.object(cluster, 'core') as mock_core:
+        with mock.patch.object(cluster, 'core_api') as mock_core:
             cluster.scale_down([])
 
             mock_core.delete_namespaced_pod.assert_not_called()
@@ -82,12 +135,12 @@ class TestCluster(unittest.TestCase):
             mock_pod2,
         ])
 
-        with mock.patch.object(cluster, 'core') as mock_core:
+        with mock.patch.object(cluster, 'core_api') as mock_core:
             mock_core.list_namespaced_pod.return_value = namespaced_pods
 
             cluster.scale_down(['192.168.0.1', '192.168.0.2'])
 
-            mock_core.list_namespaced_pod.assert_called_with('default')
+            mock_core.list_namespaced_pod.assert_called_with('default', label_selector='app=dask,component=dask-worker')
 
             mock_core.delete_namespaced_pod.assert_called_with('dask-worker-test2', 'default')
 
@@ -100,7 +153,7 @@ class TestCluster(unittest.TestCase):
 
         cluster = Cluster('default', mock_pod)
 
-        with mock.patch.object(cluster, 'core') as mock_core:
+        with mock.patch.object(cluster, 'core_api') as mock_core:
             mock_core.create_namespaced_pod.side_effect = client.rest.ApiException()
 
             with self.assertRaises(client.rest.ApiException):
@@ -115,7 +168,7 @@ class TestCluster(unittest.TestCase):
 
         cluster = Cluster('default', mock_pod)
 
-        with mock.patch.object(cluster, 'core') as mock_core:
+        with mock.patch.object(cluster, 'core_api') as mock_core:
             cluster.scale_up(2)
 
             mock_core.create_namespaced_pod.assert_any_call('default', mock_pod)
@@ -124,4 +177,4 @@ class TestCluster(unittest.TestCase):
     def test_from_yaml(self, config):
         cluster = Cluster.from_yaml('default', 'tests/worker-spec.yml')
 
-        self.assertIsNotNone(cluster.pod)
+        self.assertIsNotNone(cluster.pod_template)
