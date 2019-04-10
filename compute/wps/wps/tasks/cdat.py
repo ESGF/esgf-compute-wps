d19f834fc041cdaed6ffe1a785e8ab39d41781ef
Jason Boutte <boutte.jason@gmail.com>
2019-04-10 19:05:13 +0000

    Cleans up the cdat tasks

--- compute/wps/wps/tasks/cdat.py
+++ compute/wps/wps/tasks/cdat.py
@@ -1,13 +1,9 @@
 #! /usr/bin/env python
 
-from builtins import str
-import datetime
-import os
 from collections import OrderedDict
 
 import cdms2
 import cwt
-import cdutil
 import dask
 import dask.array as da
 import xarray as xr
@@ -17,7 +13,6 @@ from django.conf import settings
 from dask.distributed import Client
 from distributed.protocol.serialize import register_serialization
 
-from wps import metrics
 from wps import WPSError
 from wps.tasks import base
 from wps.tasks import credentials
@@ -121,170 +116,6 @@ whose value will be used to process over. The value should be a "|" delimited
 string e.g. 'lat|lon'.
 """
 
-
-def process_data(self, context, index, process):
-    """ Process a chunks of data.
-
-    Function passed as process should accept two arguments, the first being a
-    cdms2.TransientVariable and the second a list of axis names.
-
-    Args:
-        context (OperationContext): Current context.
-        index (int): Worker index used to determine the portion of work to complete.
-        process (function): A function to process the data, see above for arguments.
-
-    Returns:
-        Updated context.
-    """
-    axes = context.operation.get_parameter('axes', True)
-
-    nbytes = 0
-
-    for input_index, input in enumerate(context.sorted_inputs()):
-        for _, chunk_index, chunk in input.chunks(index, context):
-            nbytes += chunk.nbytes
-
-            process_filename = '{}_{:08}_{:08}_{}.nc'.format(
-                str(context.job.id), input_index, chunk_index, '_'.join(axes.values))
-
-            cache_path = context.gen_cache_path(process_filename)
-
-            if process is not None:
-                # Track processing time
-                with metrics.WPS_PROCESS_TIME.labels(context.operation.identifier).time():
-                    chunk = process(chunk, axes.values)
-
-            with context.new_output(cache_path) as outfile:
-                outfile.write(chunk, id=input.variable.var_name)
-
-            input.process.append(cache_path)
-
-    self.status('Processed {!r} bytes', nbytes)
-
-    return context
-
-
-def parse_filename(path):
-    """ Parses filename from path.
-
-    /data/hello.nc -> hello
-
-    Args:
-        path (string): A path to parse.
-
-    Retunrs:
-        str: The base filename
-    """
-    base = os.path.basename(path)
-
-    filename, _ = os.path.splitext(base)
-
-    return filename
-
-
-def regrid_chunk(context, chunk, selector):
-    """ Regrids a chunk of data.
-
-    Args:
-        context (OperationContext): Current context.
-        chunk (cdms2.TransientVariable): Chunk of data to be regridded.
-        selector (dict): A dict describing the portion of data we want.
-
-    Returns:
-        cdms2.TransientVariable: Regridded varibale.
-    """
-    grid, tool, method = context.regrid_context(selector)
-
-    shape = chunk.shape
-
-    chunk = chunk.regrid(grid, regridTool=tool, regridMethod=method)
-
-    logger.info('Regrid %r -> %r', shape, chunk.shape)
-
-    return chunk
-
-
-@base.cwt_shared_task()
-def concat(self, contexts):
-    """ Concatenate data chunks.
-
-    Args:
-        context (OperationContext): Current context.
-
-    Returns:
-        Updated context.
-    """
-    context = OperationContext.merge_ingress(contexts)
-
-    context.output_path = context.gen_public_path()
-
-    nbytes = 0
-    start = datetime.datetime.now()
-
-    with context.new_output(context.output_path) as outfile:
-        for index, input in enumerate(context.sorted_inputs()):
-            data = []
-            chunk_axis = None
-            chunk_axis_index = None
-
-            # Skip file if not mapped
-            if input.mapped is None:
-                logger.info('Skipping %r', input.filename)
-
-                continue
-
-            for file_path, _, chunk in input.chunks(input_index=index, context=context):
-                logger.info('Chunk shape %r %r', file_path, chunk.shape)
-
-                if chunk_axis is None:
-                    chunk_axis_index = chunk.getAxisIndex(input.chunk_axis)
-
-                    chunk_axis = chunk.getAxis(chunk_axis_index)
-
-                # We can write chunks along the temporal axis immediately
-                # otherwise we need to collect them to concatenate over
-                # an axis
-                if chunk_axis.isTime():
-                    logger.info('Writing temporal chunk %r', chunk.shape)
-
-                    if context.units is not None:
-                        chunk.getTime().toRelativeTime(str(context.units))
-
-                    if context.is_regrid:
-                        chunk = regrid_chunk(context, chunk, input.mapped)
-
-                    outfile.write(chunk, id=str(input.variable.var_name))
-                else:
-                    logger.info('Gathering spatial chunk')
-
-                    data.append(chunk)
-
-                nbytes += chunk.nbytes
-
-            # Concatenate chunks along an axis
-            if chunk_axis is not None and not chunk_axis.isTime():
-                data = MV2.concatenate(data, axis=chunk_axis_index)
-
-                if context.is_regrid:
-                    chunk = regrid_chunk(context, chunk, input.mapped)
-
-                outfile.write(data, id=str(input.variable.var_name))
-
-                nbytes += chunk.nbytes
-
-    elapsed = datetime.datetime.now() - start
-
-    self.status('Processed {!r} bytes in {!r} seconds', nbytes, elapsed.total_seconds())
-
-    return context
-
-
-@base.register_process('CDAT', 'regrid', abstract=REGRID_ABSTRACT, metadata={'inputs': '1'})
-@base.cwt_shared_task()
-def regrid(self, context, index):
-    """ Regrids a chunk of data.
-    """
-    return context
 
 variable = {
     'v0': cwt.Variable('http://esgf-data.ucar.edu/thredds/dodsC/esg_dataroot/CMIP6/CMIP/NCAR/CESM2/amip/r1i1p1f1/day/tas/gn/v20190218/tas_day_CESM2_amip_r1i1p1f1_gn_19500101-19591231.nc', 'tas', name='v0'),
@@ -762,78 +593,34 @@ def aggregate_func(self, context):
     return context
 
 
+@base.register_process('CDAT', 'regrid', abstract=REGRID_ABSTRACT, metadata={'inputs': '1'})
+@base.cwt_shared_task()
+def regrid_func(self, context):
+    return context
+
+
 @base.register_process('CDAT', 'average', abstract=AVERAGE_ABSTRACT, metadata={'inputs': '1'})
 @base.cwt_shared_task()
-def average(self, context, index):
-    def average_func(data, axes):
-        axis_indices = []
-
-        for axis in axes:
-            axis_index = data.getAxisIndex(axis)
-
-            if axis_index == -1:
-                raise WPSError('Unknown axis {!s}', axis)
-
-            axis_indices.append(str(axis_index))
-
-        axis_sig = ''.join(axis_indices)
-
-        data = cdutil.averager(data, axis=axis_sig)
-
-        return data
-
-    return process_data(self, context, index, average_func)
+def average_func(self, context):
+    return context
 
 
 @base.register_process('CDAT', 'sum', abstract=SUM_ABSTRACT, metadata={'inputs': '1'})
 @base.cwt_shared_task()
-def sum(self, context, index):
-    def sum_func(data, axes):
-        for axis in axes:
-            axis_index = data.getAxisIndex(axis)
-
-            if axis_index == -1:
-                raise WPSError('Unknown axis {!s}', axis)
-
-            data = MV2.sum(data, axis=axis_index)
-
-        return data
-
-    return process_data(self, context, index, sum_func)
+def sum_func(self, context):
+    return context
 
 
 @base.register_process('CDAT', 'max', abstract=MAX_ABSTRACT, metadata={'inputs': '1'})
 @base.cwt_shared_task()
-def max(self, context, index):
-    def max_func(data, axes):
-        for axis in axes:
-            axis_index = data.getAxisIndex(axis)
-
-            if axis_index == -1:
-                raise WPSError('Unknown axis {!s}', axis)
-
-            data = MV2.max(data, axis=axis_index)
-
-        return data
-
-    return process_data(self, context, index, max_func)
+def max_func(self, context):
+    return context
 
 
 @base.register_process('CDAT', 'min', abstract=MIN_ABSTRACT, metadata={'inputs': '1'})
 @base.cwt_shared_task()
-def min(self, context, index):
-    def min_func(data, axes):
-        for axis in axes:
-            axis_index = data.getAxisIndex(axis)
-
-            if axis_index == -1:
-                raise WPSError('Unknown axis {!s}', axis)
-
-            data = MV2.min(data, axis=axis_index)
-
-        return data
-
-    return process_data(self, context, index, min_func)
+def min_func(self, context):
+    return context
 
 
 def serialize_transient_axis(axis):
