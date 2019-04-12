ad1866f77c1717a6247702882f68b8de64215593
Jason Boutte <boutte.jason@gmail.com>
2019-04-12 21:41:07 +0000

    Removes the InlineJob view for users. Reduces loading times.

--- compute/wps/wps/admin.py
+++ compute/wps/wps/admin.py
@@ -1,49 +1,42 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from django.db.models import OuterRef, Subquery
 
 from wps import models
+
 
 class UserProcessInline(admin.TabularInline):
     model = models.UserProcess
     extra = 0
 
+
 class UserFileInline(admin.TabularInline):
     model = models.UserFile
     extra = 0
 
+
 class AuthInline(admin.TabularInline):
     model = models.Auth
 
-class JobInline(admin.TabularInline):
-    model = models.Job
-    extra = 0
-
-    readonly_fields = ('status', 'updated')
-
-    def status(self, instance):
-        return instance.status_set.latest('updated_date').status
-
-    def updated(self, instance):
-        return instance.status_set.latest('updated_date').updated_date
 
 class UserAdmin(UserAdmin):
     inlines = (
         AuthInline,
         UserFileInline,
         UserProcessInline,
-        JobInline,
     )
+
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
 
 class OpenIDNonceAdmin(admin.ModelAdmin):
     list_display = ('server_url', 'timestamp', 'salt')
 
     def has_add_permission(self, request):
         return False
+
 
 class OpenIDAssociationAdmin(admin.ModelAdmin):
     list_display = ('server_url', 'handle', 'issued', 'lifetime',
@@ -52,8 +45,10 @@ class OpenIDAssociationAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
+
 admin.site.register(models.OpenIDNonce, OpenIDNonceAdmin)
 admin.site.register(models.OpenIDAssociation, OpenIDAssociationAdmin)
+
 
 class ServerAdmin(admin.ModelAdmin):
     list_display = ('host', 'added_date', 'status')
@@ -61,17 +56,20 @@ class ServerAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
+
 class ProcessAdmin(admin.ModelAdmin):
     list_display = ('identifier', 'backend')
 
     def has_add_permission(self, request):
         return False
 
+
 class FileAdmin(admin.ModelAdmin):
     list_display = ('name', 'host', 'variable', 'url', 'requested')
 
     def has_add_permission(self, request):
         return False
+
 
 admin.site.register(models.Server, ServerAdmin)
 admin.site.register(models.Process, ProcessAdmin)
