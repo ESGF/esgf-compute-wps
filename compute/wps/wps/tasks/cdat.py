613497fa3c2945a26b755f6b1056a379ba544a92
Jason Boutte <boutte.jason@gmail.com>
2019-04-30 00:40:07 +0000

    Updates the CDAT operations to make adding new ones a little more simple

--- compute/wps/wps/tasks/cdat.py
+++ compute/wps/wps/tasks/cdat.py
@@ -116,39 +116,40 @@ def workflow_func(self, context):
 
             # If the current process is in the function map then apply the process
             if next.identifier in PROCESS_FUNC_MAP:
-                PROCESS_FUNC_MAP[next.identifier](next, input)
+                output = PROCESS_FUNC_MAP[next.identifier](next, input)
+            else:
+                output = input
 
             # Regrid to the target grid
             if next.gridder is not None:
-                regrid(next, input)
+                regrid(next, output)
 
             # Add to intermediate store
-            interm[next.name] = input
+            interm[next.name] = output
         else:
             # Grab all the intermediate inputs
             try:
-                data = [interm[x.name] for x in next.inputs]
+                inputs = [interm[x.name] for x in next.inputs]
             except KeyError as e:
                 raise WPSError('Unable to find intermidiate {!s}', e)
 
-            if len(data) > 1:
-                raise WPSError('Multiple inputs not supported')
-            else:
-                data = data[0]
+            # Create copy of single input, multiple inputs automatically create a new output
+            if len(inputs) == 1:
+                inputs = [inputs[0].copy(), ]
 
-            # Make a new copy
-            input = data.copy()
-
-            # Apply processing if needed
-            if next.identifier in PROCESS_FUNC_MAP:
-                PROCESS_FUNC_MAP[next.identifier](next, input)
+            try:
+                output = PROCESS_FUNC_MAP[next.identifier](next, *inputs)
+            except KeyError as e:
+                raise WPSError('Operation {!s} is not supported in a workflow', e)
+            except Exception:
+                raise
 
             # Regrid to target grid
             if next.gridder is not None:
-                regrid(next, input)
+                regrid(next, output)
 
             # Add new input to intermediate store
-            interm[next.name] = input
+            interm[next.name] = output
 
     # Initialize the cluster resources
     manager = init(context, settings.DASK_WORKERS)
@@ -178,87 +179,7 @@ def workflow_func(self, context):
     return context
 
 
-REGRID_ABSTRACT = """
-Regrids a variable to designated grid. Required parameter named "gridder".
-"""
-
-SUBSET_ABSTRACT = """
-Subset a variable by provided domain. Supports regridding.
-"""
-
-AGGREGATE_ABSTRACT = """
-Aggregate a variable over multiple files. Supports subsetting and regridding.
-"""
-
-AVERAGE_ABSTRACT = """
-Computes the average over axes.
-
-Required parameters:
- axes: A list of axes to operate on. Should be separated by "|".
-
-Optional parameters:
- weightoptions: A string whos value is "generate",
-   "equal", "weighted", "unweighted". See documentation
-   at https://cdat.llnl.gov/documentation/utilities/utilities-1.html
-"""
-
-SUM_ABSTRACT = """
-Computes the sum over an axis. Requires singular parameter named "chunked_axis, axes"
-whose value will be used to process over. The value should be a "|" delimited
-string e.g. 'lat|lon'.
-"""
-
-MAX_ABSTRACT = """
-Computes the maximum over an axis. Requires singular parameter named "chunked_axis, axes"
-whose value will be used to process over. The value should be a "|" delimited
-string e.g. 'lat|lon'.
-"""
-
-MIN_ABSTRACT = """
-Computes the minimum over an axis. Requires singular parameter named "chunked_axis, axes"
-whose value will be used to process over. The value should be a "|" delimited
-string e.g. 'lat|lon'.
-"""
-
-
-def process_multiple(context, process):
-    manager = init(context, settings.DASK_WORKERS)
-
-    fm = managers.FileManager(context.user)
-
-    inputs = []
-
-    for x in context.inputs:
-        input = managers.InputManager.from_cwt_variable(fm, x)
-
-        input.subset(context.domain)
-
-        inputs.append(input)
-
-    output = process(context.operation, *inputs)
-
-    if context.is_regrid:
-        regrid(context.operation, output)
-
-    dataset = output.to_xarray()
-
-    local_path = context.build_output_variable(output.var_name)
-
-    logger.info('Writing output to %r', local_path)
-
-    try:
-        delayed = dataset.to_netcdf(local_path, compute=False)
-
-        fut = manager.client.compute(delayed)
-
-        DaskJobTracker(context, fut)
-    except Exception as e:
-        raise WPSError('Error executing process: {!r}', e)
-
-    return context
-
-
-def process_single(context, process=None, aggregate=False):
+def process(context, process=None, aggregate=False):
     """ Process a single variable.
 
     Initialize the cluster workers, create the dask graph then execute.
@@ -279,23 +200,26 @@ def process_single(context, process=None, aggregate=False):
     fm = managers.FileManager(context.user)
 
     if aggregate:
-        input = managers.InputManager.from_cwt_variables(fm, context.inputs)
+        inputs = [managers.InputManager.from_cwt_variables(fm, context.inputs), ]
     else:
-        input = managers.InputManager.from_cwt_variable(fm, context.inputs[0])
+        inputs = [managers.InputManager.from_cwt_variable(fm, x) for x in context.inputs]
 
-    input.subset(context.domain)
+    for input in inputs:
+        input.subset(context.domain)
 
-    context.job.update('Subset data shape {!r}', input.data.shape)
+        context.job.update('Subset data shape {!r}', input.data.shape)
 
     if process is not None:
-        process(context.operation, input)
+        output = process(context.operation, *inputs)
+    else:
+        output = inputs[0]
 
     if context.is_regrid:
-        regrid(context.operation, input)
+        regrid(context.operation, output)
 
-    dataset = input.to_xarray()
+    dataset = output.to_xarray()
 
-    local_path = context.build_output_variable(input.var_name)
+    local_path = context.build_output_variable(output.var_name)
 
     logger.info('Writing output to %r', local_path)
 
@@ -312,23 +236,38 @@ def process_single(context, process=None, aggregate=False):
     return context
 
 
-@base.register_process('CDAT', 'subset', abstract=SUBSET_ABSTRACT, metadata={'inputs': '1'})
+SUBSET_ABSTRACT = """
+Subset a variable by provided domain. Supports regridding.
+"""
+
+
+@base.register_process('CDAT', 'subset', abstract=SUBSET_ABSTRACT, inputs=1)
 @base.cwt_shared_task()
 def subset_func(self, context):
     """ Subset Celery task.
     """
-    return process_single(context, None)
+    return process(context)
 
 
-@base.register_process('CDAT', 'aggregate', abstract=AGGREGATE_ABSTRACT, metadata={'inputs': '*'})
+AGGREGATE_ABSTRACT = """
+Aggregate a variable over multiple files. Supports subsetting and regridding.
+"""
+
+
+@base.register_process('CDAT', 'aggregate', abstract=AGGREGATE_ABSTRACT, inputs=50)
 @base.cwt_shared_task()
 def aggregate_func(self, context):
     """ Aggregate Celery task.
     """
-    return process_single(context, None, True)
+    return process(context, aggregate=True)
 
 
-@base.register_process('CDAT', 'regrid', abstract=REGRID_ABSTRACT, metadata={'inputs': '1'})
+REGRID_ABSTRACT = """
+Regrids a variable to designated grid. Required parameter named "gridder".
+"""
+
+
+@base.register_process('CDAT', 'regrid', abstract=REGRID_ABSTRACT, inputs=1)
 @base.cwt_shared_task()
 def regrid_func(self, context):
     """ Regrid Celery task.
@@ -336,7 +275,26 @@ def regrid_func(self, context):
     # Ensure gridder is provided
     context.operation.get_parameter('gridder', True)
 
-    return process_single(context, None)
+    return process(context)
+
+
+AXES_PARAM = 'axes: A list of axes to operate on. Multiple values should be separated by "|" e.g. "lat|lon".'
+
+
+CONST_PARAM = 'constant: An integer or float value that will be applied element-wise.'
+
+
+AVERAGE_ABSTRACT = """
+Computes the average over axes.
+
+Required parameters:
+ axes: A list of axes to operate on. Should be separated by "|".
+
+Optional parameters:
+ weightoptions: A string whos value is "generate",
+   "equal", "weighted", "unweighted". See documentation
+   at https://cdat.llnl.gov/documentation/utilities/utilities-1.html
+"""
 
 
 # @base.register_process('CDAT', 'average', abstract=AVERAGE_ABSTRACT, metadata={'inputs': '1'})
@@ -347,48 +305,93 @@ def average_func(self, context):
     return context
 
 
-@base.register_process('CDAT', 'sum', abstract=SUM_ABSTRACT, metadata={'inputs': '1'})
+SUM_ABSTRACT = """
+Computes the sum over an axis, two inputs or a constant.
+
+Optional parameters:
+ {!s}
+ {!s}
+""".format(AXES_PARAM, CONST_PARAM)
+
+
+@base.register_process('CDAT', 'sum', abstract=SUM_ABSTRACT, inputs=2)
 @base.cwt_shared_task()
 def sum_func(self, context):
     """ Sum Celery task.
     """
-    return process_single(context, PROCESS_FUNC_MAP['CDAT.sum'])
+    return process(context, PROCESS_FUNC_MAP['CDAT.sum'])
 
 
-@base.register_process('CDAT', 'max', abstract=MAX_ABSTRACT, metadata={'inputs': '1'})
+MAX_ABSTRACT = """
+Computes the max over an axis, two inputs or a constant.
+
+Optional parameters:
+ {!s}
+ {!s}
+""".format(AXES_PARAM, CONST_PARAM)
+
+
+@base.register_process('CDAT', 'max', abstract=MAX_ABSTRACT, inputs=2)
 @base.cwt_shared_task()
 def max_func(self, context):
     """ Max Celery task.
     """
-    return process_single(context, PROCESS_FUNC_MAP['CDAT.max'])
+    return process(context, PROCESS_FUNC_MAP['CDAT.max'])
 
 
-@base.register_process('CDAT', 'min', abstract=MIN_ABSTRACT, metadata={'inputs': '1'})
+MIN_ABSTRACT = """
+Computes the min over an axis, two inputs or a constant.
+
+Optional parameters:
+ {!s}
+ {!s}
+""".format(AXES_PARAM, CONST_PARAM)
+
+
+@base.register_process('CDAT', 'min', abstract=MIN_ABSTRACT, inputs=2)
 @base.cwt_shared_task()
 def min_func(self, context):
     """ Min Celery task.
     """
-    return process_single(context, PROCESS_FUNC_MAP['CDAT.min'])
+    return process(context, PROCESS_FUNC_MAP['CDAT.min'])
 
 
-@base.register_process('CDAT', 'subtract', abstract="", metadata={'inputs': '2'})
+SUBTRACT_ABSTRACT = """
+Computes subtract between two inputs or a constant.
+
+Optional parameters:
+ {!s}
+""".format(CONST_PARAM)
+
+
+@base.register_process('CDAT', 'subtract', abstract=SUBTRACT_ABSTRACT, inputs=2)
 @base.cwt_shared_task()
 def subtract_func(self, context):
     """ Subtract Celery task.
     """
-    return process_multiple(context, PROCESS_FUNC_MAP['CDAT.subtract'])
+    return process(context, PROCESS_FUNC_MAP['CDAT.subtract'])
 
 
-@base.register_process('CDAT', 'add', abstract="", metadata={'inputs': '2'})
+ADD_ABSTRACT = """
+Computes add between two inputs or a constant.
+
+Optional parameters:
+ {!s}
+""".format(CONST_PARAM)
+
+
+@base.register_process('CDAT', 'add', abstract=ADD_ABSTRACT, inputs=2)
 @base.cwt_shared_task()
 def add_func(self, context):
     """ Subtract Celery task.
     """
-    return process_multiple(context, PROCESS_FUNC_MAP['CDAT.add'])
+    return process(context, PROCESS_FUNC_MAP['CDAT.add'])
 
 
-def regrid(operation, input):
+def regrid(operation, *inputs):
     gridder = operation.get_parameter('gridder', True)
+
+    input = inputs[0]
 
     if len(input.vars) == 0:
         input.load_variables_and_axes()
@@ -423,9 +426,47 @@ def regrid(operation, input):
     input.vars['lon_bnds'] = input.axes['lon'].getBounds()
 
 
-def process_single_input(operation, input, process_func):
-    axes = operation.get_parameter('axes', True).values
+def process_input(operation, *inputs, process_func=None, **supported):
+    axes = operation.get_parameter('axes')
 
+    constant = operation.get_parameter('constant')
+
+    if axes is not None:
+        if not supported.get('single_input_axes', True):
+            raise WPSError('Axes parameter is not supported by operation {!r}', operation.identifier)
+
+        output = process_single_input(axes.values, process_func, inputs[0])
+    elif constant is not None:
+        if not supported.get('single_input_constant', True):
+            raise WPSError('Constant parameter is not supported by operation {!r}', operation.identifier)
+
+        try:
+            constant = float(constant.values[0])
+        except ValueError:
+            raise WPSError('Invalid constant value {!r} type {!s} expecting <class \'float\'>',
+                           constant, type(constant))
+
+        output = inputs[0]
+
+        output.data = process_func(output.data, constant)
+    elif len(inputs) > 1:
+        if not supported.get('multiple_input', True):
+            raise WPSError('Multiple inputs are not supported by operation {!r}', operation.identifier)
+
+        output = process_multiple_input(process_func, *inputs)
+    else:
+        logger.debug('Axes %r Constant %r Supported %r', axes, constant, supported)
+
+        logger.debug('Process func %r', process_func)
+
+        logger.debug('Inputs %r', inputs)
+
+        raise WPSError('Could not handle processing input')
+
+    return output
+
+
+def process_single_input(axes, process_func, input):
     map_keys = list(input.map.keys())
 
     indices = tuple(map_keys.index(x) for x in axes)
@@ -441,24 +482,36 @@ def process_single_input(operation, input, process_func):
 
         input.map.pop(axis)
 
+    return input
 
-def process_multiple_input(operation, input1, input2, process_func):
+
+def process_multiple_input(process_func, input1, input2):
     input1.load_variables_and_axes()
 
     new_input = input1.copy()
 
-    new_input.data = process_func(input1.data, input2.data)
+    if process_func.__name__ in REQUIRES_STACK:
+        stacked = da.stack([input1.data, input2.data])
 
-    new_input.data.visualize()
+        new_input.data = process_func(stacked, axis=0)
+    else:
+        new_input.data = process_func(input1.data, input2.data)
 
     return new_input
 
 
+REQUIRES_STACK = [
+    da.sum.__name__,
+    da.max.__name__,
+    da.min.__name__,
+]
+
+
 PROCESS_FUNC_MAP = {
-    'CDAT.sum': partial(process_single_input, process_func=da.sum),
-    'CDAT.max': partial(process_single_input, process_func=da.max),
-    'CDAT.min': partial(process_single_input, process_func=da.min),
+    'CDAT.sum': partial(process_input, process_func=da.sum),
+    'CDAT.max': partial(process_input, process_func=da.max),
+    'CDAT.min': partial(process_input, process_func=da.min),
     'CDAT.regrid': regrid,
-    'CDAT.subtract': partial(process_multiple_input, process_func=da.subtract),
-    'CDAT.add': partial(process_multiple_input, process_func=da.add),
+    'CDAT.subtract': partial(process_input, process_func=da.subtract, single_input_axes=False),
+    'CDAT.add': partial(process_input, process_func=da.add, singe_input_axes=False),
 }
