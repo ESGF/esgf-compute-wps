558f718dde1d79e3ca1f62ae071fdeed1dd97187
Jason Boutte <boutte.jason@gmail.com>
2019-05-16 01:27:56 +0000

    Fixes sending job status emails based on django model signals

--- compute/compute_tasks/compute_tasks/job.py
+++ compute/compute_tasks/compute_tasks/job.py
@@ -4,8 +4,6 @@ import json
 
 import cwt
 from celery.utils.log import get_task_logger
-from django.conf import settings
-from django.core.mail import EmailMessage
 
 from compute_tasks import base
 from compute_tasks import WPSError
@@ -13,93 +11,6 @@ from compute_tasks.context import OperationContext
 from compute_tasks.context import WorkflowOperationContext
 
 logger = get_task_logger('compute_tasks.job')
-
-JOB_SUCCESS_MSG = """
-Hello {name},
-
-<pre>
-Job {job} has finished successfully.
-</pre>
-
-<pre>
-{outputs}
-</pre>
-
-<pre>
-<a href="{settings.WPS_JOB_URL}">View job history</a>
-</pre>
-
-<pre>
-Thank you,
-ESGF Compute Team
-</pre>
-"""
-
-JOB_FAILED_MSG = """
-Hello {name},
-
-<pre>
-Job {job} has failed.
-</pre>
-
-<pre>
-{error}
-</pre>
-
-Report errors on <a href="https://github.com/ESGF/esgf-compute-wps/issues/new?template=Bug_report.md">GitHub</a>.
-
-<pre>
-Thank you,
-ESGF Compute Team
-</pre>
-"""
-
-
-def send_failed_email(context, error):
-    user = context.user_details()
-
-    msg = JOB_FAILED_MSG.format(name=user['first_name'], job=context.job, error=error)
-
-    email = EmailMessage('Job Failed', msg, to=[user['email'], ])
-
-    email.content_subtype = 'html'
-
-    email.send(fail_silently=True)
-
-
-def send_success_email(context, variable):
-    logger.info('Sending success email using variable %r', variable)
-
-    user = context.user_details()
-
-    if not isinstance(variable, (list, tuple)):
-        variable = [variable, ]
-
-    outputs = '\n'.join('<a href="{!s}.html">{!s}|{!s}</a>'.format(
-        x.uri,
-        x.var_name,
-        x.name)
-        for x in variable)
-
-    msg = JOB_SUCCESS_MSG.format(name=user['first_name'], job=context.job, settings=settings, outputs=outputs)
-
-    email = EmailMessage('Job Success', msg, to=[user['email'], ])
-
-    email.content_subtype = 'html'
-
-    email.send(fail_silently=True)
-
-
-def send_success_email_data(context, outputs):
-    user = context.user_details()
-
-    msg = JOB_SUCCESS_MSG.format(name=user['first_name'], job=context.job, settings=settings, outputs=outputs)
-
-    email = EmailMessage('Job Success', msg, to=[user['email'], ])
-
-    email.content_subtype = 'html'
-
-    email.send(fail_silently=True)
 
 
 def build_context(identifier, data_inputs):
@@ -164,16 +75,10 @@ def job_succeeded(self, context):
     if len(context.output) == 1:
         if isinstance(context.output[0], cwt.Variable):
             context.succeeded(json.dumps(context.output[0].to_dict()))
-
-            send_success_email(context, context.output)
         elif isinstance(context.output[0], str):
             context.succeeded(context.output)
-
-            send_success_email_data(context, context.output)
     else:
-        context.job.succeeded(json.dumps([x.to_dict() for x in context.output]))
-
-        send_success_email(context, context.output)
+        context.succeeded(json.dumps([x.to_dict() for x in context.output]))
 
     from compute_tasks import context as ctx
 
--- compute/wps/wps/signals.py
+++ compute/wps/wps/signals.py
@@ -1,12 +1,97 @@
 import os
 import logging
 
-from wps import models
-
+from django.conf import settings
+from django.core.mail import EmailMessage
 from django.db.models.signals import post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from wps import models
+
 logger = logging.getLogger('wps.signals')
+
+JOB_SUCCESS_MSG = """
+Hello {first_name},
+
+<pre>
+Job {job_id} has finished successfully.
+</pre>
+
+<pre>
+<a href="{job_url}">View job history</a>
+</pre>
+
+<pre>
+Thank you,
+ESGF Compute Team
+</pre>
+"""
+
+JOB_FAILED_MSG = """
+Hello {first_name},
+
+<pre>
+Job {job_id} has failed.
+</pre>
+
+<pre>
+{exception}
+</pre>
+
+Report errors on <a href="https://github.com/ESGF/esgf-compute-wps/issues/new?template=Bug_report.md">GitHub</a>.
+
+<pre>
+Thank you,
+ESGF Compute Team
+</pre>
+"""
+
+
+def send_failed_email(job, exception):
+    logger.info('Sending failed email to user %r', job.user.id)
+
+    kwargs = {
+        'first_name': job.user.first_name,
+        'job_id': job.id,
+        'exception': exception,
+    }
+
+    msg = JOB_FAILED_MSG.format(**kwargs)
+
+    email = EmailMessage('Job Failed', msg, to=[job.user.email, ])
+
+    email.content_subtype = 'html'
+
+    email.send(fail_silently=True)
+
+
+def send_success_email(job):
+    logger.info('Sending success email to user %r', job.user.pk)
+
+    kwargs = {
+        'first_name': job.user.first_name,
+        'job_id': job.id,
+        'job_url': settings.WPS_JOB_URL,
+    }
+
+    msg = JOB_SUCCESS_MSG.format(**kwargs)
+
+    email = EmailMessage('Job Success', msg, to=[job.user.email, ])
+
+    email.content_subtype = 'html'
+
+    email.send(fail_silently=True)
+
+
+@receiver(post_save, sender=models.Status)
+def save_job_handler(sender, instance, **kwargs):
+    logger.info('Handling job status %r', instance.status)
+
+    if instance.status == 'ProcessSucceeded':
+        send_success_email(instance.job)
+    elif instance.status == 'ProcessFailed':
+        send_failed_email(instance.job, instance.exception)
 
 
 @receiver(post_delete, sender=models.Output)
