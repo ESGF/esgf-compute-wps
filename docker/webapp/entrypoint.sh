dea3df3dac5fb1264c0bca1086157dd1ffd94b4e
Jason Boutte <boutte.jason@gmail.com>
2019-07-17 19:44:03 -0700

    Updates the webapp image

--- docker/webapp/entrypoint.sh
+++ docker/webapp/entrypoint.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-./node_modules/.bin/http-server ./assets/ -p 80 >/dev/null 2>&1 &
-
-./node_modules/.bin/webpack --config config/webpack.dev.js --watch
+./node_modules/.bin/light-server -s ./assets/ -p 80 -w "src/**/*.ts,src/**/*.html,src/**/*.css # ./node_modules/.bin/webpack --config config/webpack.dev.js # reload" --historyindex "/index.html"
