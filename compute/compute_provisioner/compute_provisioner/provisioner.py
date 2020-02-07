e54397260d78383db89ddc71df70f10dd02bc884
Jason Boutte <boutte.jason@gmail.com>
2020-02-07 07:25:19 +0000

    Fixes changing sha256 object to str

--- compute/compute_provisioner/compute_provisioner/provisioner.py
+++ compute/compute_provisioner/compute_provisioner/provisioner.py
@@ -270,7 +270,7 @@ class Provisioner(threading.Thread):
         Args:
             request: A str containing a list of YAML definition of k8s resources.
         """
-        resource_uuid = hashlib.sha256(request_raw)
+        resource_uuid = hashlib.sha256(request_raw).hexdigest()
 
         request = json.loads(request_raw)
 
