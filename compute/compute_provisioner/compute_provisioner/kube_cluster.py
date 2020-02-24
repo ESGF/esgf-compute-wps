3bcdcfd04b4d73170db7062daeafcce4c098bfba
Jason Boutte <boutte.jason@gmail.com>
2020-02-24 17:48:40 +0000

    Updates the resource monitor to handle new namespace allocation.

--- compute/compute_provisioner/compute_provisioner/kube_cluster.py
+++ compute/compute_provisioner/compute_provisioner/kube_cluster.py
@@ -6,6 +6,8 @@ import redis
 from kubernetes import client
 from kubernetes import config
 
+from compute_provisioner.provisioner import KubernetesAllocator
+
 logger = logging.getLogger('provisioner.kube_cluster')
 
 
@@ -23,118 +25,41 @@ class KubeCluster(threading.Thread):
 
         self.ignore_lifetime = ignore_lifetime
 
-        config.load_incluster_config()
-
-        self.core = client.CoreV1Api()
-
-        self.apps = client.AppsV1Api()
-
-        self.exts = client.ExtensionsV1beta1Api()
+        self.k8s = KubernetesAllocator()
 
         self.redis = redis.Redis(self.redis_host, db=1)
 
-    def check_rogue_expired_resources(self, kind, selector, resource, delete_func):
-        logger.info('Checking %r %r resources', len(resource.items), kind)
+    def check_resources(self):
+        label_selector = 'compute.io/resource-group'
 
-        for x in resource.items:
-            key = x.metadata.labels[selector]
+        ns = self.k8s.list_namespace(label_selector=label_selector)
+
+        logger.info(f'Checking {len(ns.items)!s} namespaces')
+
+        for x in ns.items:
+            key = x.metadata.labels[label_selector]
 
             expire = self.redis.hget('resource', key)
 
             if expire is None:
-                logger.info('Removing rogue resource %r %r', kind, x.metadata.name)
+                logger.info(f'Removing non-tracked namespace {x.metadata.name!r}')
 
                 if not self.dry_run:
-                    delete_func(x.metadata.name, self.namespace)
-
-                    logger.info('Removed %r %r', kind, x.metadata.name)
+                    self.k8s.delete_namespace(x.metadata.name)
             else:
                 expire = float(expire.decode())
 
-                logger.info('Resource %r set to expire in %r', x.metadata.name, expire-time.time())
+                logger.info(f'Checking if namespace {x.metadata.name!r} is expired')
 
                 if expire < time.time() or self.ignore_lifetime:
-                    logger.info('Removing expired resource %r %r', kind, x.metadata.name)
+                    logger.info(f'Namespace {x.metadata.name!r} expired')
 
                     if not self.dry_run:
-                        delete_func(x.metadata.name, self.namespace)
-
-                        logger.info('Removed %r %r', kind, x.metadata.name)
+                        self.k8s.delete_namespace(x.metadata.name)
 
                         self.redis.hdel('resource', key)
-                else:
-                    logger.debug('Found validate resource %r %r', kind, x.metadata.name)
-
-    def remove_resource(self, resource, delete_func):
-        for x in resource.items:
-            try:
-                delete_func(x.metadata.name, self.namespace)
-            except Exception:
-                # Resource should get cleaned up
-                logger.exception('Failed to delete resource')
-
-    def check_resources(self):
-        keys_to_remove = []
-
-        for resource_uuid, expire in self.redis.hscan_iter('resource'):
-            logger.info(f'Checking resource uuid {resource_uuid!r} expire {expire!r}')
-
-            selector = 'app.kubernetes.io/resource-group-uuid={!s}'.format(resource_uuid.decode())
-
-            expire = float(expire)
-
-            if time.time() <= expire:
-                logger.info('Resource not expired')
-
-                continue
-
-            resource = self.apps.list_namespaced_deployment(self.namespace, label_selector=selector)
-
-            self.remove_resource(resource, self.apps.delete_namespaced_deployment)
-
-            resource = self.core.list_namespaced_pod(self.namespace, label_selector=selector)
-
-            self.remove_resource(resource, self.core.delete_namespaced_pod)
-
-            resource = self.core.list_namespaced_service(self.namespace, label_selector=selector)
-
-            self.remove_resource(resource, self.core.delete_namespaced_service)
-
-            resource = self.exts.list_namespaced_ingress(self.namespace, label_selector=selector)
-
-            self.remove_resource(resource, self.exts.delete_namespaced_ingress)
-
-            keys_to_remove.append(resource_uuid)
-
-        logger.info(f'Removing keys {keys_to_remove!r}')
-
-        if len(keys_to_remove) > 0:
-            self.redis.hdel('resource', *keys_to_remove)
-
-        logger.info('Done checking for resources')
-
-    def validate_existing_resources(self):
-        selector = 'app.kubernetes.io/resource-group-uuid'
-
-        pods = self.core.list_namespaced_pod(self.namespace, label_selector=selector)
-
-        self.check_rogue_expired_resources('Pod', selector, pods, self.core.delete_namespaced_pod)
-
-        deployments = self.apps.list_namespaced_deployment(self.namespace, label_selector=selector)
-
-        self.check_rogue_expired_resources('Deployment', selector, deployments, self.apps.delete_namespaced_deployment)
-
-        services = self.core.list_namespaced_service(self.namespace, label_selector=selector)
-
-        self.check_rogue_expired_resources('Service', selector, services, self.core.delete_namespaced_service)
-
-        ingresses = self.exts.list_namespaced_ingress(self.namespace, label_selector=selector)
-
-        self.check_rogue_expired_resources('Ingress', selector, ingresses, self.exts.delete_namespaced_ingress)
 
     def monitor(self):
-        self.validate_existing_resources()
-
         while True:
             self.check_resources()
 
