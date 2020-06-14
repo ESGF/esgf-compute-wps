799186d754b69c568eb67a7c04a7b3705ceb2216
Jason Boutte <boutte.jason@gmail.com>
2020-06-13 17:02:53 -0700

    Optimizes gunicorns flags for container use

--- compute/compute_wps/entrypoint.sh
+++ compute/compute_wps/entrypoint.sh
@@ -1,3 +1,8 @@
 #! /bin/bash 
 
-gunicorn -b 0.0.0.0:8000 compute.wsgi $@
+gunicorn -b 0.0.0.0:8000 \
+  --worker-tmp-dir /dev/shm \
+  --workers 2 \
+  --threads 4 \
+  --worker-class gthread \
+  compute.wsgi
