258186ddc0f8c39e73539ecf1c3d96a4ff5a028f
Jason Boutte <boutte.jason@gmail.com>
2020-06-24 00:19:35 -0700

    Updates thredds container

--- docker/thredds/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Make directory incase it is not already present
-[[ ! -e "/data/public" ]] && mkdir -p /data/public
-
-$@
--- docker/thredds/setenv.sh
+++ docker/thredds/setenv.sh
@@ -1,19 +1,14 @@
-#$!/bin/sh
+#!/bin/sh
 
 export CATALINA_HOME="/usr/local/tomcat"
 export CATALINA_BASE="/usr/local/tomcat"
 
 CONTENT_ROOT=-Dtds.content.root.path=/usr/local/tomcat/content
 
-JAVA_PREFS_ROOTS="-Djava.util.prefs.systemRoot=$CATALINA_HOME/content/thredds/javaUtilPrefs \
-                  -Djava.util.prefs.userRoot=$CATALINA_HOME/content/thredds/javaUtilPrefs"
+JAVA_PREFS_ROOTS="-Djava.util.prefs.systemRoot=$CATALINA_HOME/content/threddsCWT/javaUtilPrefs \
+                  -Djava.util.prefs.userRoot=$CATALINA_HOME/content/threddsCWT/javaUtilPrefs"
 
-echo "Java heap init ${JAVA_HEAP_INIT} max ${JAVA_HEAP_MAX}"
-
-NORMAL="-d64 -Xmx${JAVA_HEAP_MAX} -Xms${JAVA_HEAP_INIT} -server -XX:MaxPermSize=256m"
-HEAD_DUMP="-XX:+HeapDumpOnOutOfMemoryError"
-HEADLESS="-Djava.awt.headless=true"
-
-JAVA_OPTS="$CONTENT_ROOT $NORMAL $MAX_PERM_GEN $HEAP_DUMP $HEADLESS $JAVA_PREFS_ROOT"
+JAVA_OPTS="$CONTENT_ROOT -d64 -server -Djava.awt.headless=true -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0"
+JAVA_OPTS="$JAVA_OPTS $JAVA_PREFS_ROOT"
 
 export JAVA_OPTS
