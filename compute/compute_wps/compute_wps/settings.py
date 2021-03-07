a704ac45be51d8cebfca2049d634e847c8d84cab
Jason Boutte <boutte.jason@gmail.com>
2021-03-06 19:41:41 -0800

    Fixes static files configuration

--- compute/compute_wps/compute_wps/settings.py
+++ compute/compute_wps/compute_wps/settings.py
@@ -257,11 +257,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = '/var/www/static'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
-)
+STATICFILES_DIRS = ()
 
 LOGGING = {
     'version': 1,
